aa9131cfec662016132af0696a0d0b7af8a92064
Kex <kexanone@gmail.com>
2024-03-03 11:06:18 +0100

    Add description to example

--- addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
+++ addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
@@ -3,6 +3,6 @@
 [BaseContainerProps()]
 class ACE_Medical_Settings : ACE_ModSettings
 {
-	[Attribute("0")]
+	[Attribute(defvalue: "0", desc: "Medical kits can only be used with supplies if enabled")]
 	bool m_bEnableSuppliesForMedicalKit;
 }

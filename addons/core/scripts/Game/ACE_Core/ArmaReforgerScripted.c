eb4c9a6d60ab1c719b799343bfdd2b5a820d91fb
Kex <kexanone@gmail.com>
2024-03-03 11:53:40 +0100

    Make variable names of type ACE_SettingsConfig more concise

--- addons/core/scripts/Game/ACE_Core/ArmaReforgerScripted.c
+++ addons/core/scripts/Game/ACE_Core/ArmaReforgerScripted.c
@@ -3,23 +3,23 @@
 //! Created when the game starts (not when a world starts!) and persists until the game is closed.
 modded class ArmaReforgerScripted : ChimeraGame
 {
-	static protected ref ACE_SettingsConfig m_ACE_Settings;
+	static protected ref ACE_SettingsConfig m_ACE_SettingsConfig;
 	
 	//------------------------------------------------------------------------------------------------
 	//! Load settings when starting the game for the first time
 	override bool OnGameStart()
 	{
-		if (!m_ACE_Settings)
+		if (!m_ACE_SettingsConfig)
 		{
 			// Load settings from config file
-			m_ACE_Settings = SCR_ConfigHelperT<ACE_SettingsConfig>.GetConfigObject("{A305FEB7400A2965}Configs/ACE/Settings.conf");
-			if (!m_ACE_Settings)
+			m_ACE_SettingsConfig = SCR_ConfigHelperT<ACE_SettingsConfig>.GetConfigObject("{A305FEB7400A2965}Configs/ACE/Settings.conf");
+			if (!m_ACE_SettingsConfig)
 				return false;
 			
 			// Override with settings present in mission header
 			SCR_MissionHeader missionHeader = SCR_MissionHeader.Cast(GetGame().GetMissionHeader());
 			if (missionHeader)
-				missionHeader.ACE_ApplyToSettingsConfig(m_ACE_Settings);
+				missionHeader.ACE_ApplyToSettingsConfig(m_ACE_SettingsConfig);
 		};
 		
 		return super.OnGameStart();
@@ -27,8 +27,8 @@ modded class ArmaReforgerScripted : ChimeraGame
 	
 	//------------------------------------------------------------------------------------------------
 	//! Return all settings
-	static ACE_SettingsConfig ACE_GetSettings()
+	static ACE_SettingsConfig ACE_GetSettingsConfig()
 	{
-		return m_ACE_Settings;
+		return m_ACE_SettingsConfig;
 	}
 }

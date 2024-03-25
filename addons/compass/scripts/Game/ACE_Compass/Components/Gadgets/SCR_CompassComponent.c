1b21db4e438dc12cd4e8b7307861f8e13b574ade
Kex <kexanone@gmail.com>
2024-03-25 18:49:31 +0100

    Refactor, make sure update is only run on owner and deactivate display when compass stops updating

--- addons/compass/scripts/Game/ACE_Compass/Components/Gadgets/SCR_CompassComponent.c
+++ addons/compass/scripts/Game/ACE_Compass/Components/Gadgets/SCR_CompassComponent.c
@@ -1,21 +1,17 @@
 //------------------------------------------------------------------------------------------------
 modded class SCR_CompassComponent : SCR_GadgetComponent
 {	
-	protected IEntity m_pACE_Compass_Entity;
 	protected ACE_Compass_Display m_ACE_Compass_Display;
 	
-	//------------------------------------------------------------------------------------------------
-	override void EOnInit(IEntity owner)
-	{
-		super.EOnInit(owner);
-		m_pACE_Compass_Entity = owner;
-	}
-
 	//------------------------------------------------------------------------------------------------
 	override void ActivateGadgetUpdate()
 	{
 		super.ActivateGadgetUpdate();
-
+		
+		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
+			return;
+		
+		// https://discord.com/channels/105462288051380224/976155351999201390/1138453903562768414
 		if (!m_ACE_Compass_Display)
 			m_ACE_Compass_Display = ACE_Compass_Display.Cast(SCR_HUDManagerComponent.GetHUDManager().FindInfoDisplay(ACE_Compass_Display));
 	}
@@ -24,30 +20,40 @@ modded class SCR_CompassComponent : SCR_GadgetComponent
 	override void Update(float timeSlice)
 	{
 		super.Update(timeSlice);
-		bool isDisplayShown = m_ACE_Compass_Display.IsShown();
+		
+		// Only run on owner
+		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
+			return;
+		
+		bool displayShown = m_ACE_Compass_Display.IsShown();
 
 		if (m_bIsInMapMode || !m_bFocused)
 		{
-			if (isDisplayShown)
-			{
+			if (displayShown)
 				m_ACE_Compass_Display.Show(false);
-			};
 
 			return;
 		}
 
-		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
-			return;
-
-		// https://discord.com/channels/105462288051380224/976155351999201390/1138453903562768414
-		if (!isDisplayShown)
+		if (!displayShown)
 		{
-			m_ACE_Compass_Display.SetCompassEntity(m_pACE_Compass_Entity);
+			m_ACE_Compass_Display.SetCompassEntity(GetOwner());
 			m_ACE_Compass_Display.Show(true, UIConstants.FADE_RATE_SLOW);
 		}
-		else
-		{
-			m_ACE_Compass_Display.UpdateBearing(m_fNeedleAngle);
-		}
+		
+		m_ACE_Compass_Display.UpdateBearing(m_fNeedleAngle);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Handles the cases when a player falls unconscious while having the compass focused
+	override void DeactivateGadgetUpdate()
+	{
+		super.DeactivateGadgetUpdate();
+		
+		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
+			return;
+		
+		if (m_ACE_Compass_Display && m_ACE_Compass_Display.IsShown())
+			m_ACE_Compass_Display.Show(false);
 	}
 }

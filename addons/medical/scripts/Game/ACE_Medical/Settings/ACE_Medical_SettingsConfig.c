37266c1846530178f35674025b91a0aaaaffe6bd
Kex <kexanone@gmail.com>
2024-03-28 00:13:47 +0100

    Update addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>

--- addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
+++ addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
@@ -3,6 +3,6 @@
 [BaseContainerProps()]
 class ACE_Medical_Settings : ACE_ModSettings
 {
-	[Attribute(defvalue: "0", desc: "Enables second chance on headshots")]
-	float m_bSecondChanceOnHeadEnabled;
+	[Attribute(defvalue: "false", desc: "Enables second chance on headshots")]
+	bool m_bSecondChanceOnHeadEnabled;
 }

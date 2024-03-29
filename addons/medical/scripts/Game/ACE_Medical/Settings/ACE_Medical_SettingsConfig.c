f37dc4351ad6d38a2efe598ab5e7e0c39a82dd14
Kex <kexanone@gmail.com>
2024-03-29 10:09:01 +0100

    Medical - Add settings for medical kit  (#14)
    * Add settings for medical kit max healing and supply usage
    
    * Update medical settings
    
    * Remove tmp folder

--- addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
+++ addons/medical/scripts/Game/ACE_Medical/Settings/ACE_Medical_SettingsConfig.c
@@ -3,6 +3,9 @@
 [BaseContainerProps()]
 class ACE_Medical_Settings : ACE_ModSettings
 {
-	[Attribute(defvalue: "0", desc: "Medical kits can only be used with supplies if enabled")]
-	bool m_bEnableSuppliesForMedicalKit;
+	[Attribute(defvalue: "1", desc: "Healing consumes supplies when enabled. Ignored when global supply usage is disabled.")]
+	bool m_bHealSupplyUsageEnabled;
+	
+	[Attribute(defvalue: "0.5", desc: "Maximum scaled health (from 0 to 1) that a medical kit can heal to outside of medical facilities")]
+	float m_fMedicalKitMaxHealScaled;
 }

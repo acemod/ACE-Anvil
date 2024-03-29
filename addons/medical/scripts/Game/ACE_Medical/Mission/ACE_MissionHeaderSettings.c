f29a5dc06aeab546f15ebf41cfe6d2a50a4a651c
Kex <kexanone@gmail.com>
2024-03-29 09:38:52 +0100

    Basic set-up with settings framework (#3)
    * Add all-in-one mod
    
    * Add common mod with settings framework
    
    * Add medical mod with example of applying settings framework
    
    * Rename common to core
    
    * Remove src folder
    
    * Add medical to all-in-one
    
    * Simplify and unify names for classes and methods
    
    * Make mission header members protected and remove duplicate modded
    
    * Fix settings.conf for medical
    
    * Add description to example
    
    * Add missing s in settings
    
    * Make variable names of type ACE_SettingsConfig more concise
    
    * Remove medical prefab overrides
    
    * Use settings array only for init and move the refs to the map
    
    * Add notnull

---
+++ addons/medical/scripts/Game/ACE_Medical/Mission/ACE_MissionHeaderSettings.c
@@ -0,0 +1,16 @@
+//------------------------------------------------------------------------------------------------
+//! Add mod settings to mission header
+[BaseContainerProps()]
+modded class ACE_MissionHeaderSettings
+{
+	[Attribute()]
+	protected ref ACE_Medical_Settings m_ACE_Medical_Settings;
+	
+	//------------------------------------------------------------------------------------------------
+	//! Applies settings from mission header to config
+	override void ApplyToSettingsConfig(notnull ACE_SettingsConfig config)
+	{
+		if (m_ACE_Medical_Settings)
+			config.SetModSettings(m_ACE_Medical_Settings);
+	}
+}

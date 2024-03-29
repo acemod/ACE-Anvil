b938f03975ca206aa63485587309614f40bdaaf4
Kex <kexanone@gmail.com>
2024-03-29 10:08:44 +0100

    Medical - Add Epinephrine (#7)
    * Add EpiPen
    
    * Add EpiPen to medics and arsenal
    
    * Use GetLifeState in favor of IsUnconscious
    
    * Fix supply costs
    
    * EpiPen interaction to legs
    
    * Make "not incapacitated" a separate fail reason
    
    * Double regen rate of EpiPen
    
    * Add epinephrine to RHS arsenals and medics
    
    * Prevent epinephrine from being applied when too injured
    
    * Fix stringtable runtime GUIDs
    
    * Update addons/medical/scripts/Game/ACE_Medical/Components/Damage/SCR_CharacterDamageManagerComponent.c
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>

---
+++ addons/medical/scripts/Game/ACE_Medical/Components/Gadgets/SCR_ConsumableEffectBase.c
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------------------------
+//! Type of consumable gadget
+modded enum SCR_EConsumableType
+{
+	ACE_MEDICAL_EPINEPHRINE
+};
+
+//------------------------------------------------------------------------------------------------
+//! Reason why consumable cannot be applied
+modded enum SCR_EConsumableFailReason
+{
+	ACE_MEDICAL_NOT_INCAPACITATED,
+	ACE_MEDICAL_TOO_DAMAGED
+}

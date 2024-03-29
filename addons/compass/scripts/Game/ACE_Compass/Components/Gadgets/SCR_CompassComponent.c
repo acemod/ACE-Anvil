d89fd4198f15134834e8f2aa984c7f1f3cf1c6ed
Kex <kexanone@gmail.com>
2024-03-29 10:34:43 +0100

    Add Compass (#13)
    * Add Compass addon, HUD proto
    
    * Add Core as dependency
    
    * Implement Zfill method
    
    * Add ACE_CompassTools
    
    * Implement compass display handlers
    
    * Rename and refactor modded SCR_CompassComponent
    
    * Add compass display and disable it on creation
    
    * Move core scripts to core folder
    
    * Update project ID
    
    * Update bearing from held compass, make HUD track 3d pos of the compass
    
    * Fix hud not vanishing if when ADS is toggled quickly
    
    * Update HUD layout
    
    * Move and refactor scripts
    
    * Remove Zfill
    
    * Update cardinal
    
    * Use module for mapping bearings
    
    * Put widget on compass needle bone
    
    * Add outline to text
    
    * Refactor, make sure update is only run on owner and deactivate display when compass stops updating
    
    ---------
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>

---
+++ addons/compass/scripts/Game/ACE_Compass/Components/Gadgets/SCR_CompassComponent.c
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------------------------
+modded class SCR_CompassComponent : SCR_GadgetComponent
+{	
+	protected ACE_Compass_Display m_ACE_Compass_Display;
+	
+	//------------------------------------------------------------------------------------------------
+	override void ActivateGadgetUpdate()
+	{
+		super.ActivateGadgetUpdate();
+		
+		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
+			return;
+		
+		// https://discord.com/channels/105462288051380224/976155351999201390/1138453903562768414
+		if (!m_ACE_Compass_Display)
+			m_ACE_Compass_Display = ACE_Compass_Display.Cast(SCR_HUDManagerComponent.GetHUDManager().FindInfoDisplay(ACE_Compass_Display));
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void Update(float timeSlice)
+	{
+		super.Update(timeSlice);
+		
+		// Only run on owner
+		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
+			return;
+		
+		bool displayShown = m_ACE_Compass_Display.IsShown();
+
+		if (m_bIsInMapMode || !m_bFocused)
+		{
+			if (displayShown)
+				m_ACE_Compass_Display.Show(false);
+
+			return;
+		}
+
+		if (!displayShown)
+		{
+			m_ACE_Compass_Display.SetCompassEntity(GetOwner());
+			m_ACE_Compass_Display.Show(true, UIConstants.FADE_RATE_SLOW);
+		}
+		
+		m_ACE_Compass_Display.UpdateBearing(m_fNeedleAngle);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Handles the cases when a player falls unconscious while having the compass focused
+	override void DeactivateGadgetUpdate()
+	{
+		super.DeactivateGadgetUpdate();
+		
+		if (m_CharacterOwner != SCR_PlayerController.GetLocalControlledEntity())
+			return;
+		
+		if (m_ACE_Compass_Display && m_ACE_Compass_Display.IsShown())
+			m_ACE_Compass_Display.Show(false);
+	}
+}

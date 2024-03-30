36ac7a95dea27405e1122fb94c76b6080d707ac2
Kex <kexanone@gmail.com>
2024-03-30 03:41:22 +0100

    Finger - Add CEN Tactical Ping (#10)
    * Add CEN - Tactical Ping
    
    * Update addons/finger/scripts/Game/ACE_Finger/Editor/Components/Editor/ACE_Finger_EditorComponent.c
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>
    
    * Update addons/finger/scripts/Game/ACE_Finger/Editor/UI/InfoDisplays/SCR_EditorPingInfoDisplay.c
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>

---
+++ addons/finger/scripts/Game/ACE_Finger/Network/Notifications/ENotification.c
@@ -0,0 +1,7 @@
+//------------------------------------------------------------------------------------------------
+modded enum ENotification
+{
+	ACE_FINGER_PING, //<- Notification when pointing at a terrain position
+	ACE_FINGER_PING_TARGET_ENTITY //<- Notification when pointing at an object
+};
+

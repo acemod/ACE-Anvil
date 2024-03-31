06298623e76c942df92e8e0f5172ad6dda3230a6
bux <github@jonathandavid.de>
2024-03-31 11:54:32 +0200

    add config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,31 +2,33 @@ import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import tailwind from '@astrojs/tailwind'
 
+import { config } from './src/utils/config'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'ACE Anvil',
+      title: config.site.name,
       customCss: [
         // Path to your Tailwind base styles:
-        './src/tailwind.css',
+        './src/styles/tailwind.css',
       ],
       social: {
-        discord: 'https://acemod.org/discord',
-        github: 'https://github.com/acemod/ACE-Anvil',
+        'discord': 'https://acemod.org/discord',
+        'github': 'https://github.com/acemod/ACE-Anvil',
         'x.com': 'https://twitter.com/ACE3Mod',
-        facebook: 'https://facebook.com/ACE3Mod',
-        youtube: 'https://youtube.com/c/ACE3Mod',
+        'facebook': 'https://facebook.com/ACE3Mod',
+        'youtube': 'https://youtube.com/c/ACE3Mod',
       },
       sidebar: [
         {
           label: 'Guides',
           autogenerate: { directory: 'guides' },
-          //items: [
+          // items: [
           //  // Each item here is one entry in the navigation menu.
           //  { label: 'Example Guide', link: '/guides/example/' },
           //  { label: 'Development', link: '/guides/development/' },
-          //],
+          // ],
         },
         {
           label: 'Features',

b6429ea6931e9041ecba594d245296b97a0ab3fa
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-04-09 01:35:58 +0200

    Instantiate m_ACE_EditorStruct only if it's null
    It should be restored by Deserialization

--- addons/core/scripts/Game/ACE_Core/GameMode/SaveLoad/SCR_MissionStruct.c
+++ addons/core/scripts/Game/ACE_Core/GameMode/SaveLoad/SCR_MissionStruct.c
@@ -5,12 +5,13 @@ modded class SCR_MissionStruct: SCR_JsonApiStruct
 	protected ref SCR_JsonApiStruct m_ACE_EditorStruct;
 	
 	void SCR_MissionStruct()
-	{		
-		m_ACE_EditorStruct = new ACE_EditorStruct();
+	{
+		RegV("m_ACE_EditorStruct");
+		
+		if (!m_ACE_EditorStruct)
+			m_ACE_EditorStruct = new ACE_EditorStruct();
 		
 		m_aStructs.Insert(m_ACE_EditorStruct);
 		m_aStructTypes.Insert(m_ACE_EditorStruct.Type());
-		
-		RegV("m_ACE_EditorStruct");
 	}
 }

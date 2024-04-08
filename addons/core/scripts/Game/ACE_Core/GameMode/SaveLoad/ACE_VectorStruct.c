5ecdad4ff0ea6ac4fcc2ac83f5b15ced1ad7a0df
Filip Maciejewski <veteran29@users.noreply.github.com>
2024-04-09 01:35:21 +0200

    Shorten name of serialized prop in ACE_VectorStruct
    Better space usage

--- addons/core/scripts/Game/ACE_Core/GameMode/SaveLoad/ACE_VectorStruct.c
+++ addons/core/scripts/Game/ACE_Core/GameMode/SaveLoad/ACE_VectorStruct.c
@@ -2,18 +2,19 @@
 //! JSON helper struct for storing vectors in an array
 class ACE_VectorStruct : SCR_JsonApiStruct
 {
-	protected vector m_vVector;
+	// short name for more efficient serialization
+	protected vector v;
 	
 	//------------------------------------------------------------------------------------------------
-	void ACE_VectorStruct(vector v)
+	void ACE_VectorStruct(vector vec)
 	{
-		RegV("m_vVector");
-		m_vVector = v;
+		RegV("v");
+		v = vec;
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	vector GetVector()
 	{
-		return m_vVector;
+		return v;
 	}
 }

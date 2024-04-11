56fc0e74137b3f362f21738ae77f89f3c7e9a66b
PabstMirror <pabstmirror@gmail.com>
2024-04-11 16:36:56 -0500

    Update clangFormat.py

--- tools/clangFormat.py
+++ tools/clangFormat.py
@@ -1,36 +1,39 @@
 import os
+import shutil
 import sys
 import subprocess
 import concurrent.futures
-import tomllib
 
 addons_base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "addons")
 
-# todo: check path or llvm but this works for now if you have vs
-clang_tidy_exe = os.path.join(r"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-format.exe")
+clang_tidy_exe = exe = shutil.which("clang-format") # https://github.com/llvm/llvm-project/releases
+if clang_tidy_exe is None: clang_tidy_exe = os.path.join(r"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-format.exe")
 
-def process():
-    for root, _dirs, files in os.walk(addons_base_path):
-        for file in files:
-            if file.endswith(".c"):
-                filePath = os.path.join(root, file)
-                fakePath = filePath + "s"
-                print(f"proccsing {filePath}")
-
-                os.rename(filePath, fakePath)
-
-                cmd = [clang_tidy_exe, "-i", fakePath]
-                ret = subprocess.call(cmd)
-
-                os.rename(fakePath, filePath)
-    return 0
-
+def process_file(filePath):
+    fakePath = filePath + "s"
+    os.rename(filePath, fakePath)
+    ret = subprocess.call([clang_tidy_exe, "-i", fakePath])
+    os.rename(fakePath, filePath)
+    return [filePath, ret]
 
 def main():
     if not os.path.isfile(clang_tidy_exe):
         print(f"Error: not found {clang_tidy_exe}")
         return 1
-    return process()
+    
+    arma_files = []
+    for root, _dirs, files in os.walk(addons_base_path):
+        for file in files:
+            if file.endswith(".c"): 
+                arma_files.append(os.path.join(root, file))
+
+    print("clanging {} .c arma files".format(len(arma_files)))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
+        for filePath, ret in executor.map(process_file, arma_files):
+            if ret != 0:
+                print(f"error {ret} with {filePath}")
+
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())

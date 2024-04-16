2177c435e9d5d48806b1fb24faa78a893fefae88
Kex <kexanone@gmail.com>
2024-04-17 01:59:46 +0200

    Cache resource names with associated config entries

--- addons/chopping/scripts/Game/ACE_Chopping/Configs/ACE_Chopping_Config.c
+++ addons/chopping/scripts/Game/ACE_Chopping/Configs/ACE_Chopping_Config.c
@@ -5,38 +5,57 @@ class ACE_Chopping_Config
 	[Attribute(desc: "Maps plant base prefab to its corresponding chopping helper entity")]
 	protected ref array<ref ACE_Chopping_ConfigEntry> m_aEntries;
 	
+	protected ref map<ResourceName, ACE_Chopping_ConfigEntry> m_mResourceNameCache = new map<ResourceName, ACE_Chopping_ConfigEntry>();
+	
 	//----------------------------------------------------------------------------------------
 	//! Spawns chopping helper entity for the corrsponding plant on the local player
 	IEntity SpawnHelper(IEntity plant)
+	{
+		ACE_Chopping_ConfigEntry entry = GetConfigEntry(plant);
+		if (!entry)
+			return null;
+
+		Resource res = Resource.Load(entry.m_sHelperPrefabName);
+		if (!res.IsValid())
+			return null;
+				
+		EntitySpawnParams params = new EntitySpawnParams();
+		params.TransformMode = ETransformMode.WORLD;
+		plant.GetWorldTransform(params.Transform);
+		params.Transform[3] = params.Transform[3] + entry.m_vOffset;
+				
+		ACE_Chopping_HelperEntity helper = ACE_Chopping_HelperEntity.Cast(GetGame().SpawnEntityPrefabLocal(res, plant.GetWorld(), params));
+		if (!helper)
+			return null;
+				
+		helper.SetAssociatedPlant(plant);
+		return helper;
+	}
+	
+	//----------------------------------------------------------------------------------------
+	protected ACE_Chopping_ConfigEntry GetConfigEntry(IEntity plant)
 	{
 		EntityPrefabData prefabData = plant.GetPrefabData();
 		if (!prefabData)
 			return null;
 		
+		ResourceName resName = prefabData.GetPrefabName();
+		
+		ACE_Chopping_ConfigEntry cachedEntry;
+		if (m_mResourceNameCache.Find(resName, cachedEntry))
+			return cachedEntry;
+		
 		BaseContainer container = prefabData.GetPrefab();
 		if (!container)
 			return null;
 		
-		foreach (ACE_Chopping_ConfigEntry entry: m_aEntries)
+		foreach (ACE_Chopping_ConfigEntry entry : m_aEntries)
 		{
-			if (!SCR_BaseContainerTools.IsKindOf(container, entry.m_sPlantPrefabName))
-				continue;
-
-			Resource res = Resource.Load(entry.m_sHelperPrefabName);
-			if (!res.IsValid())
-				continue;
-				
-			EntitySpawnParams params = new EntitySpawnParams();
-			params.TransformMode = ETransformMode.WORLD;
-			plant.GetWorldTransform(params.Transform);
-			params.Transform[3] = params.Transform[3] + entry.m_vOffset;
-				
-			ACE_Chopping_HelperEntity helper = ACE_Chopping_HelperEntity.Cast(GetGame().SpawnEntityPrefabLocal(res, plant.GetWorld(), params));
-			if (!helper)
-				continue;
-				
-			helper.SetAssociatedPlant(plant);
-			return helper;
+			if (SCR_BaseContainerTools.IsKindOf(container, entry.m_sPlantPrefabName))
+			{
+				m_mResourceNameCache.Insert(resName, entry);
+				return entry;
+			}
 		}
 		
 		return null;

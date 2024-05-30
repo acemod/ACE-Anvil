f9b0ade723f1fb7eed5a95205fec216391b5b9b6
Kveberg <tkvebbs@gmail.com>
2024-05-30 23:03:43 +0200

    Update addons/magrepack/scripts/Game/ACE_MagRepack/Player/SCR_PlayerController.c
    Co-authored-by: Kex <kexanone@gmail.com>

--- addons/magrepack/scripts/Game/ACE_MagRepack/Player/SCR_PlayerController.c
+++ addons/magrepack/scripts/Game/ACE_MagRepack/Player/SCR_PlayerController.c
@@ -68,7 +68,8 @@ modded class SCR_PlayerController
 				if (!managerComp.TryInsertItemInStorage(toEntity, managerComp.FindStorageForItem(toEntity, EStoragePurpose.PURPOSE_ANY)))
 				{
 					Print("Was unable to successfully toMag back into storage. If it could not be droppeed on the ground, it was deleted.", LogLevel.WARNING);
-					if (!managerComp.TryRemoveItemFromInventory(toEntity)) {SCR_EntityHelper.DeleteEntityAndChildren(toEntity);}
+					if (!managerComp.TryRemoveItemFromInventory(toEntity)) 
+						SCR_EntityHelper.DeleteEntityAndChildren(toEntity);
 				}
 			}
 

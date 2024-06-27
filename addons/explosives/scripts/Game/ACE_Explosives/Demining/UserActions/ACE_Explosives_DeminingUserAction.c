ee74ac9e8c17e32412a9e924b4e874939927c1e6
DerivenLoki <mikial00@outlook.com>
2024-06-27 19:55:32 -0400

    Demining Action for Landmines
    Adds a Shovel prompt to disassemble landmines placed by players. Also makes landmines immune to Gunfire, in favor of this new change.

---
+++ addons/explosives/scripts/Game/ACE_Explosives/Demining/UserActions/ACE_Explosives_DeminingUserAction.c
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------------------------
+//! Mine deletion user action
+class ACE_Explosives_DeminingUserAction : ACE_ShovelDestroyUserAction
+{
+	
+	//------------------------------------------------------------------------------------------------
+	//! Request deletion of the tree
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity) 
+	{
+		SCR_EntityHelper.DeleteEntityAndChildren(pOwnerEntity);
+	}
+}

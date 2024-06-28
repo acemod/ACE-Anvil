89c5788b145011d5d5df31c1ec7103de811b5de5
DerivenLoki <mikial00@outlook.com>
2024-06-27 20:36:58 -0400

    Update - Minor Tweaks
    Tweaked the UserActions to appropriately reflect the new Class
    
    Tweaked the Radius of the Action
    
    Moved "HasLocalEffectOnlyScript" to Chopping_UserAction from ShovelDestroyUserAction

--- addons/chopping/scripts/Game/ACE_Chopping/UserActions/ACE_Chopping_UserAction.c
+++ addons/chopping/scripts/Game/ACE_Chopping/UserActions/ACE_Chopping_UserAction.c
@@ -2,24 +2,31 @@
 //! Tree deletion user action
 class ACE_Chopping_UserAction : ACE_ShovelDestroyUserAction
 {
-	
-	//------------------------------------------------------------------------------------------------
-	//! Request deletion of the tree
-	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity) 
-	{
-		SCR_PlayerController userCtrl = SCR_PlayerController.Cast(GetGame().GetPlayerController());
-		if (!userCtrl)
-			return;
-		
-		ACE_Chopping_HelperEntity helper = ACE_Chopping_HelperEntity.Cast(GetOwner());
-		if (!helper)
-			return;
-		
-		IEntity plant = helper.GetAssociatedPlant();
-		if (!plant)
-			return;
-		
-		userCtrl.ACE_DeleteEntityAtPosition(plant.GetOrigin());
-		SCR_EntityHelper.DeleteEntityAndChildren(helper);
-	}
+    
+    //------------------------------------------------------------------------------------------------
+    //! Request deletion of the tree
+    override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity) 
+    {
+        SCR_PlayerController userCtrl = SCR_PlayerController.Cast(GetGame().GetPlayerController());
+        if (!userCtrl)
+            return;
+        
+        ACE_Chopping_HelperEntity helper = ACE_Chopping_HelperEntity.Cast(GetOwner());
+        if (!helper)
+            return;
+        
+        IEntity plant = helper.GetAssociatedPlant();
+        if (!plant)
+            return;
+        
+        userCtrl.ACE_DeleteEntityAtPosition(plant.GetOrigin());
+        SCR_EntityHelper.DeleteEntityAndChildren(helper);
+    }
+
+    //------------------------------------------------------------------------------------------------
+    //! For entities that have no RplComponent, only local scripts will work
+    override bool HasLocalEffectOnlyScript()
+    {
+        return true;
+    }
 }

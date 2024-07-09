822ad92e69aa7bb3859b2978b41e56bf5460c5fd
Kex <kexanone@gmail.com>
2024-07-10 00:47:46 +0200

    Carrying - Fixes for experimental (#65)
    * Update ECompartmentType enum and use GetOutVehicle_NoDoor
    
    * Fix crash by removing doors from helper compartment
    
    * Add carrying back to AiO
    
    * Create new minimal animation graph for carrying
    
    * Override exit and entry position info
    
    * Add ACE_MoveOutVehicle to SCR_CompartmentAccessComponent
    
    * Use ACE_MoveOutVehicle to move carried out of compartment
    
    * Update addons/core/scripts/Game/ACE_Core/Vehicle/SCR_CompartmentAccessComponent.c
    
    ---------
    
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>

---
+++ addons/core/scripts/Game/ACE_Core/Vehicle/SCR_CompartmentAccessComponent.c
@@ -0,0 +1,17 @@
+
+//------------------------------------------------------------------------------------------------
+modded class SCR_CompartmentAccessComponent : CompartmentAccessComponent
+{
+	//------------------------------------------------------------------------------------------------
+	void ACE_MoveOutVehicle(vector target_transform[4], bool sendIntoRagdoll = false, bool performWhenPaused = false)
+	{
+		Rpc(ACE_MoveOutVehicle_Owner, target_transform, sendIntoRagdoll, performWhenPaused);
+	}
+	
+	
+	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
+	protected void ACE_MoveOutVehicle_Owner(vector target_transform[4], bool sendIntoRagdoll, bool performWhenPaused)
+	{
+		GetOutVehicle_NoDoor(target_transform, false, false);
+	}
+}

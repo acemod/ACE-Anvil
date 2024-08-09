e9d31114212aa479076560ac772f9a7264b527da
Kex <kexanone@gmail.com>
2024-08-09 02:02:26 +0200

    Broadcast max heal setting to all clients

--- addons/medical/scripts/Game/ACE_Medical/Components/SupportStation/SCR_HealSupportStationComponent.c
+++ addons/medical/scripts/Game/ACE_Medical/Components/SupportStation/SCR_HealSupportStationComponent.c
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------------------------
 modded class SCR_HealSupportStationComponent : SCR_BaseDamageHealSupportStationComponent
-{	
+{
 	//------------------------------------------------------------------------------------------------
 	//! Apply settings
 	override protected void DelayedInit(IEntity owner)
@@ -18,7 +18,17 @@ modded class SCR_HealSupportStationComponent : SCR_BaseDamageHealSupportStationC
 		resource.SetResourceTypeEnabled(settings.m_bHealSupplyUsageEnabled);
 		
 		// Set how much medical kits can heal
-		if (InventoryItemComponent.Cast(owner.FindComponent(InventoryItemComponent)))
+		if (InventoryItemComponent.Cast(owner.FindComponent(InventoryItemComponent)) && m_fMaxHealScaled != settings.m_fMedicalKitMaxHealScaled)
+		{
 			m_fMaxHealScaled = settings.m_fMedicalKitMaxHealScaled;
+			Rpc(RpcDo_ACE_Medical_SetMaxHealScaledBroadcast, settings.m_fMedicalKitMaxHealScaled);
+		}	
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
+	void RpcDo_ACE_Medical_SetMaxHealScaledBroadcast(float maxHealScaled)
+	{
+		m_fMaxHealScaled = maxHealScaled;
 	}
 }

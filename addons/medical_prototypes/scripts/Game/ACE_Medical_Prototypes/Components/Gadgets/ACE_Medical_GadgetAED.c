abd55a5e7b34595d43d2a3061ccdf774b168c1b5
Hypoxic <martini_a@outlook.com>
2024-08-17 23:58:21 -0600

    Create ACE_Medical_GadgetAED.c
    initial parts of the AED

---
+++ addons/medical_prototypes/scripts/Game/ACE_Medical_Prototypes/Components/Gadgets/ACE_Medical_GadgetAED.c
@@ -0,0 +1,64 @@
+class ACE_Medical_GadgetAEDClass : GenericEntityClass
+{
+}
+
+class ACE_Medical_GadgetAED : GenericEntity
+{
+	// Constants - Defibrillation 
+	protected const int m_iAEDChargeTime = 3000;		// ms 
+	
+	// Properties - Defibrillation 
+	protected int m_iAEDUsesLeft = 999;
+	protected bool m_bAEDIsCharged = false;
+	protected float m_fAEDChargeAmount = 0;
+	
+	//------------------------------------------------------------------------------------------------
+	void ChargeAED()
+	{
+		if (m_fAEDChargeAmount < 1)
+		{
+			m_fAEDChargeAmount++;
+			GetGame().GetCallqueue().CallLater(ChargeAED, 1000, true)
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	bool IsAEDCharged()
+	{
+		if (m_fAEDChargeAmount < 1)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	float GetAEDChargeAmount()
+	{
+		return m_fAEDChargeAmount;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	bool Defibrillate(ACE_Medical_CardiovascularComponent component)
+	{		
+		if (IsAEDCharged() &&
+			IsShockableRhythm(component))
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	bool IsShockableRhythm(ACE_Medical_CardiovascularComponent component)
+	{
+		if (component.GetCardiacRhythmState() != ACE_Medical_ECardiacRhythmState.VF)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+}

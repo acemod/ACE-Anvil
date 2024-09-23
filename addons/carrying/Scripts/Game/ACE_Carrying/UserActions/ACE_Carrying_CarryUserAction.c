c082a22f79664a7c4874993ec22bc41b8d29a554
Kex <kexanone@gmail.com>
2024-09-23 22:04:54 +0200

    Carrying - Handle simultaneously carry action (#99)
    * Make sure on the server side that the owner is in fact not already carried
    
    * Use replicated variables for determining if someone is carrier/carried
    
    * Correct comment
    
    * Check on server if target in fact not carried

--- addons/carrying/Scripts/Game/ACE_Carrying/UserActions/ACE_Carrying_CarryUserAction.c
+++ addons/carrying/Scripts/Game/ACE_Carrying/UserActions/ACE_Carrying_CarryUserAction.c
@@ -54,10 +54,14 @@ class ACE_Carrying_CarryUserAction : ScriptedUserAction
 	//------------------------------------------------------------------------------------------------
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
+		// Check on server if they are in faction not yet carried
+		if (ACE_Carrying_Tools.IsCarried(pOwnerEntity))
+			return;
+		
 		ACE_Carrying_Tools.Carry(pUserEntity, pOwnerEntity);
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	//! Methods are executed on the local player
+	//! Only run PerformAction on server
 	override bool CanBroadcastScript() { return false; };
 }

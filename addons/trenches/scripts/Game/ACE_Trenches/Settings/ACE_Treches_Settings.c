dd45e3052c3d72c0f3cd7f0cf83d503b4d4a1db5
Kex <kexanone@gmail.com>
2024-10-21 22:59:41 +0200

    Update attribute description

--- addons/trenches/scripts/Game/ACE_Trenches/Settings/ACE_Treches_Settings.c
+++ addons/trenches/scripts/Game/ACE_Trenches/Settings/ACE_Treches_Settings.c
@@ -3,6 +3,6 @@
 [BaseContainerProps()]
 class ACE_Treches_Settings : ACE_ModSettings
 {
-	[Attribute(defvalue: "2", desc: "How much more time it takes to build structures placed with the E-tool [s]")]
+	[Attribute(defvalue: "2", desc: "How much more time it takes to build structures placed with the E-tool.")]
 	float m_fBuildTimeScale;
 }

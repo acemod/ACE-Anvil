e8bff03138f3ba85e1f1ffef32efb9d6561f20c3
Kex <kexanone@gmail.com>
2025-01-28 00:37:02 +0100

    Add gesture and editor context actions

---
+++ addons/captives/scripts/Game/ACE_Captives/Editor/Containers/Actions/ContextActions/ACE_Captives_ToggleEntityContextAction.c
@@ -0,0 +1,51 @@
+//------------------------------------------------------------------------------------------------
+[BaseContainerProps(), SCR_BaseContainerCustomTitleUIInfo("m_Info")]
+class ACE_Captives_ToggleEntityContextAction : SCR_SelectedEntitiesContextAction
+{
+	[Attribute(uiwidget: UIWidgets.ResourceNamePicker, desc: "Prefab name of the helper compartment", params: "et")]
+	protected ResourceName m_sHelperCompartmentPrefabName;
+	
+	//------------------------------------------------------------------------------------------------
+	override bool CanBeShown(SCR_EditableEntityComponent selectedEntity, vector cursorWorldPosition, int flags)
+	{
+		return SCR_EditableCharacterComponent.Cast(selectedEntity);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override bool CanBePerformed(SCR_EditableEntityComponent selectedEntity, vector cursorWorldPosition, int flags)
+	{
+		SCR_ChimeraCharacter char = SCR_ChimeraCharacter.Cast(selectedEntity.GetOwner());
+		if (!char)
+			return false;
+		
+		CharacterControllerComponent charCtrl = char.GetCharacterController();
+		if (!charCtrl || charCtrl.GetLifeState() != ECharacterLifeState.ALIVE)
+			return false;
+		
+		IEntity parent = selectedEntity.GetOwner().GetParent();
+		if (!parent)
+			return true;
+				
+		return ACE_AnimationHelperCompartment.Cast(parent);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void Perform(SCR_EditableEntityComponent selectedEntity, vector cursorWorldPosition)
+	{
+		IEntity char = selectedEntity.GetOwner();
+		if (!char)
+			return;
+		
+		ACE_AnimationHelperCompartment helper = ACE_AnimationHelperCompartment.Cast(char.GetParent());
+		if (helper && helper.GetPrefabData().GetPrefabName() == m_sHelperCompartmentPrefabName)
+		{
+			helper.Terminate(EGetOutType.ANIMATED);
+		}
+		else
+		{
+			vector transform[4];
+			char.GetWorldTransform(transform);
+			ACE_AnimationTools.AnimateWithHelperCompartment(char, transform, m_sHelperCompartmentPrefabName);
+		}
+	}
+}

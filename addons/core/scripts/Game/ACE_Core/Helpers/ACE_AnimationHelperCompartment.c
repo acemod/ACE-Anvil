f7ad6500cddaf69eb20f4fc61e5799dc8d7e7f81
Kex <kexanone@gmail.com>
2025-01-28 00:28:42 +0100

    Implement common animation helper compartment class in core

---
+++ addons/core/scripts/Game/ACE_Core/Helpers/ACE_AnimationHelperCompartment.c
@@ -0,0 +1,170 @@
+//------------------------------------------------------------------------------------------------
+class ACE_AnimationHelperCompartmentClass : GenericEntityClass
+{
+}
+
+//------------------------------------------------------------------------------------------------
+//! Helper compartment for performing a continous animation or pose while a character occupies it
+//! --- To Do: Create derived classes of this for carrying and CPR
+class ACE_AnimationHelperCompartment : GenericEntity
+{
+	protected IEntity m_pPerformer;
+	protected ref ScriptInvoker m_OnCompartmentLeft;
+
+	protected static const int SEARCH_POS_RADIUS_M = 5; // Search radius for safe position for dropping performer
+	protected static const float HELPER_DELETION_DELAY_MS = 1000; // Delay for helper to get deleted after release
+	
+	//------------------------------------------------------------------------------------------------
+	//! Move performer into compartment and attach handlers
+	void Init(IEntity performer)
+	{
+		m_pPerformer = performer;
+		
+		SCR_CompartmentAccessComponent compartmentAccess = SCR_CompartmentAccessComponent.Cast(performer.FindComponent(SCR_CompartmentAccessComponent));
+		if (!compartmentAccess)
+			return;
+		
+		compartmentAccess.GetOnCompartmentEntered().Insert(OnCompartmentEntered);
+		compartmentAccess.MoveInVehicle(this, ECompartmentType.CARGO);
+	}
+		
+	//------------------------------------------------------------------------------------------------
+	protected void OnCompartmentEntered(IEntity targetEntity, BaseCompartmentManagerComponent manager, int mgrID, int slotID, bool move)
+	{
+		SCR_CompartmentAccessComponent compartmentAccess = SCR_CompartmentAccessComponent.Cast(m_pPerformer.FindComponent(SCR_CompartmentAccessComponent));
+		if (!compartmentAccess)
+			return;
+		
+		compartmentAccess.GetOnCompartmentEntered().Remove(OnCompartmentEntered);
+		AttachHandlers();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void AttachHandlers()
+	{
+		// Set owner to performer
+		PlayerManager playerManager = GetGame().GetPlayerManager();
+		SCR_PlayerController performerPlayerController = SCR_PlayerController.Cast(playerManager.GetPlayerController(playerManager.GetPlayerIdFromControlledEntity(m_pPerformer)));
+		
+		RplComponent rpl = RplComponent.Cast(FindComponent(RplComponent));
+		if (performerPlayerController && rpl)
+			rpl.Give(performerPlayerController.GetRplIdentity());
+		
+		SCR_CharacterControllerComponent charController = SCR_CharacterControllerComponent.Cast(m_pPerformer.FindComponent(SCR_CharacterControllerComponent));
+		if (charController)
+			charController.m_OnLifeStateChanged.Insert(OnPerformerLifeStateChanged);
+				
+		SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+		if (gameMode)
+			gameMode.GetOnPlayerDisconnected().Insert(OnPlayerDisconnected);
+		
+		SCR_CompartmentAccessComponent compartmentAccess = SCR_CompartmentAccessComponent.Cast(m_pPerformer.FindComponent(SCR_CompartmentAccessComponent));
+		if (compartmentAccess)
+			compartmentAccess.GetOnCompartmentLeft().Insert(OnCompartmentLeft);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void DetachHandlers()
+	{
+		if (m_pPerformer)
+		{
+			SCR_CharacterControllerComponent charController = SCR_CharacterControllerComponent.Cast(m_pPerformer.FindComponent(SCR_CharacterControllerComponent));
+			if (charController)
+				charController.m_OnLifeStateChanged.Remove(OnPerformerLifeStateChanged);
+		}
+		
+		SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+		if (gameMode)
+			gameMode.GetOnPlayerDisconnected().Remove(OnPlayerDisconnected);
+		
+		// Clean-up when carried has left the compartment
+		SCR_CompartmentAccessComponent compartmentAccess = SCR_CompartmentAccessComponent.Cast(m_pPerformer.FindComponent(SCR_CompartmentAccessComponent));
+		if (compartmentAccess)
+			compartmentAccess.GetOnCompartmentLeft().Remove(OnCompartmentLeft);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Terminates CPR
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	void Terminate(EGetOutType getOutType)
+	{
+		SCR_CompartmentAccessComponent compartmentAccess = SCR_CompartmentAccessComponent.Cast(m_pPerformer.FindComponent(SCR_CompartmentAccessComponent));
+		if (!compartmentAccess)
+			return;
+		
+		if (getOutType == EGetOutType.ANIMATED)
+			compartmentAccess.ACE_GetOutVehicle(EGetOutType.ANIMATED, -1, ECloseDoorAfterActions.INVALID, false);
+		else
+			EjectPerformer();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Eject the performer out of the helper compartment
+	protected void EjectPerformer()
+	{
+		if (!m_pPerformer)
+			return;
+		
+		SCR_CompartmentAccessComponent compartmentAccess = SCR_CompartmentAccessComponent.Cast(m_pPerformer.FindComponent(SCR_CompartmentAccessComponent));
+		if (!compartmentAccess)
+			return;
+				
+		vector transform[4];
+		GetWorldTransform(transform);
+		vector pos;
+		SCR_WorldTools.FindEmptyTerrainPosition(pos, transform[3], SEARCH_POS_RADIUS_M);
+		transform[3] = pos;
+		compartmentAccess.ACE_MoveOutVehicle(transform);
+		
+		// Broadcast teleport on network
+		RplComponent performerRpl = RplComponent.Cast(m_pPerformer.FindComponent(RplComponent));
+		if (performerRpl)
+			performerRpl.ForceNodeMovement(GetOrigin());
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Clean-up when the peformer has left the compartment
+	protected void OnCompartmentLeft()
+	{
+		DetachHandlers();
+		
+		if (m_OnCompartmentLeft)
+			m_OnCompartmentLeft.Invoke(m_pPerformer);
+		
+		// Deletion of helper has to be delayed or released players stay visibly prone for other players on dedicated
+		GetGame().GetCallqueue().CallLater(SCR_EntityHelper.DeleteEntityAndChildren, HELPER_DELETION_DELAY_MS, false, this);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Terminate when performer gets incapacitated or dies
+	protected void OnPerformerLifeStateChanged(ECharacterLifeState previousLifeState, ECharacterLifeState newLifeState)
+	{
+		Terminate(EGetOutType.TELEPORT);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Terminate when patient wakes up
+	protected void OnPatientLifeStateChanged(ECharacterLifeState previousLifeState, ECharacterLifeState newLifeState)
+	{
+		if (newLifeState == ECharacterLifeState.ALIVE)
+			Terminate(EGetOutType.ANIMATED);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Terminate when performer or patient disconnects
+	protected void OnPlayerDisconnected(int playerId, KickCauseCode cause, int timeout)
+	{
+		IEntity player = GetGame().GetPlayerManager().GetPlayerControlledEntity(playerId);
+		if (player == m_pPerformer)
+			Terminate(EGetOutType.TELEPORT);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	ScriptInvoker GetOnCompartmentLeft()
+	{
+		if (!m_OnCompartmentLeft)
+			m_OnCompartmentLeft = new ScriptInvoker();
+		
+		return m_OnCompartmentLeft;
+	}
+}
--- addons/core/scripts/Game/ACE_Core/Vehicle/SCR_CompartmentAccessComponent.c
+++ addons/core/scripts/Game/ACE_Core/Vehicle/SCR_CompartmentAccessComponent.c
@@ -5,13 +5,26 @@ modded class SCR_CompartmentAccessComponent : CompartmentAccessComponent
 	//------------------------------------------------------------------------------------------------
 	void ACE_MoveOutVehicle(vector target_transform[4], bool sendIntoRagdoll = false, bool performWhenPaused = false)
 	{
-		Rpc(ACE_MoveOutVehicle_Owner, target_transform, sendIntoRagdoll, performWhenPaused);
+		Rpc(RpcDo_ACE_MoveOutVehicle_Owner, target_transform, sendIntoRagdoll, performWhenPaused);
 	}
 	
-	
+	//------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
-	protected void ACE_MoveOutVehicle_Owner(vector target_transform[4], bool sendIntoRagdoll, bool performWhenPaused)
+	protected void RpcDo_ACE_MoveOutVehicle_Owner(vector target_transform[4], bool sendIntoRagdoll, bool performWhenPaused)
 	{
 		GetOutVehicle_NoDoor(target_transform, sendIntoRagdoll, performWhenPaused);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void ACE_GetOutVehicle(EGetOutType type, int doorInfoIndex, ECloseDoorAfterActions closeDoor, bool performWhenPaused)
+	{
+		Rpc(RpcDo_ACE_GetOutVehicle_Owner, type, doorInfoIndex, closeDoor, performWhenPaused);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
+	protected void RpcDo_ACE_GetOutVehicle_Owner(EGetOutType type, int doorInfoIndex, ECloseDoorAfterActions closeDoor, bool performWhenPaused)
+	{
+		GetOutVehicle(type, doorInfoIndex, closeDoor, performWhenPaused);
 	}
 }

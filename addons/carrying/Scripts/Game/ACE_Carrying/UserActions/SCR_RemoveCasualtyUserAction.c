b7f1065ffe7f0f09b3ae6e475fe0e0ae807bb967
Kex <kexanone@gmail.com>
2025-02-26 22:58:05 +0100

    Make sure casualty gets ejected first

--- addons/carrying/Scripts/Game/ACE_Carrying/UserActions/SCR_RemoveCasualtyUserAction.c
+++ addons/carrying/Scripts/Game/ACE_Carrying/UserActions/SCR_RemoveCasualtyUserAction.c
@@ -5,10 +5,13 @@ modded class SCR_RemoveCasualtyUserAction : SCR_CompartmentUserAction
 	//! Initate carry when possible
 	override protected void PerformRemoveCasualtyAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
+		// First call the base implementation, which ejects the character from the vehicle
+		super.PerformRemoveCasualtyAction(pOwnerEntity, pUserEntity);
+		
 		BaseCompartmentSlot compartment = GetCompartmentSlot();
 		if (!compartment)
 			return;
-
+		
 		SCR_ChimeraCharacter casualtyChar = SCR_ChimeraCharacter.Cast(compartment.GetOccupant());
 		if (!casualtyChar)
 			return;
@@ -24,7 +27,5 @@ modded class SCR_RemoveCasualtyUserAction : SCR_CompartmentUserAction
 		string cannotPerformReason;
 		if (userCharController.ACE_Carrying_CanCarryCasualty(casualtyChar, cannotPerformReason))
 			userCharController.ACE_Carrying_CarryCasualty(casualtyChar);
-		else
-			super.PerformRemoveCasualtyAction(pOwnerEntity, pUserEntity);
 	}
 }

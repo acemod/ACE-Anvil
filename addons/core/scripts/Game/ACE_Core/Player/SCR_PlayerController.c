1c89c771d3702fc65e3cec500348aa9d4827086f
Kex <kexanone@gmail.com>
2025-03-30 17:47:01 +0200

    Chopping - Add tree destruction to tree removal action (#140)
    * Update passed arguments of FinishItemUse
    
    * ActionMenuFailReason no longer exists
    
    * OnInit got replaced with OnPostInit
    
    * Persistent effects getter now returns SCR_PersistentDamageEffect
    
    * Destroy tree first before deleting it
    
    * Pass EntityID to RPC instead of array<int>
    
    * Delete trunk faster
    
    * Handle delayed deletion on server

--- addons/core/scripts/Game/ACE_Core/Player/SCR_PlayerController.c
+++ addons/core/scripts/Game/ACE_Core/Player/SCR_PlayerController.c
@@ -6,17 +6,54 @@ modded class SCR_PlayerController : PlayerController
 	//! Called from local player
 	void ACE_RequestDeleteEntity(IEntity entity)
 	{
-		Rpc(RpcAsk_ACE_DeleteEntityByBits, ACE_EntityIdHelper.ToInt(entity.GetID()));
+		if (!entity)
+			return;
+		
+		Rpc(RpcAsk_ACE_DeleteEntityByID, entity.GetID());
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
-	protected void RpcAsk_ACE_DeleteEntityByBits(array<int> bits)
+	protected void RpcAsk_ACE_DeleteEntityByID(EntityID entityID)
 	{
 		ACE_LoadtimeEntityManager manager = ACE_LoadtimeEntityManager.GetInstance();
 		if (!manager)
 			return;
 		
-		manager.DeleteEntitiesByIdGlobal({EntityID.FromInt(bits[0], bits[1])});
+		manager.DeleteEntitiesByIdGlobal({entityID});
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Request destruction of SCR_DestructibleEntity
+	//! Called from local player
+	void ACE_RequestDestroyEntity(SCR_DestructibleEntity entity, EDamageType damageType, vector hitPosDirNorm[3], int deletionDelayMS = -1)
+	{
+		Rpc(RpcAsk_ACE_DestroyEntity, entity.GetID(), damageType, hitPosDirNorm, deletionDelayMS);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	protected void RpcAsk_ACE_DestroyEntity(EntityID entityID, EDamageType damageType, vector hitPosDirNorm[3], int deletionDelayMS)
+	{
+		SCR_DestructibleEntity entity = SCR_DestructibleEntity.Cast(GetGame().GetWorld().FindEntityByID(entityID));
+		if (!entity)
+			return;
+		
+		float health = entity.GetCurrentHealth();
+		if (health > 0)
+			entity.HandleDamage(damageType, health, hitPosDirNorm);
+		
+		if (deletionDelayMS <= 0)
+			return;
+		
+		ACE_LoadtimeEntityManager manager = ACE_LoadtimeEntityManager.GetInstance();
+		if (!manager)
+			return;
+		
+		// Delete immediately if it was already destroyed
+		if (health <= 0)
+			manager.DeleteEntitiesByIdGlobal({entity.GetID()});
+		else
+			GetGame().GetCallqueue().CallLater(manager.DeleteEntitiesByIdGlobal, deletionDelayMS, false, {entity.GetID()});
 	}
 }

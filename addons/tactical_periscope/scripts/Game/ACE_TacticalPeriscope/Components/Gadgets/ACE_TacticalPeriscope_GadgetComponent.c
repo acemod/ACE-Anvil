3f71b8d38ed2246bc3e9458d6c6e4db3395aa266
Kex <kexanone@gmail.com>
2025-03-30 18:04:23 +0200

    Add tactical periscope (#163)
    * Add tactical periscope
    
    * Update authors list
    
    * Add docs

---
+++ addons/tactical_periscope/scripts/Game/ACE_TacticalPeriscope/Components/Gadgets/ACE_TacticalPeriscope_GadgetComponent.c
@@ -0,0 +1,11 @@
+//-----------------------------------------------------------------------------
+class ACE_TacticalPeriscope_GadgetComponentClass : SCR_BinocularsComponentClass
+{
+}
+
+
+//-----------------------------------------------------------------------------
+//! Same as binoculars, but gets a different camera in SCR_CharacterCameraHandlerComponent
+class ACE_TacticalPeriscope_GadgetComponent : SCR_BinocularsComponent
+{
+}

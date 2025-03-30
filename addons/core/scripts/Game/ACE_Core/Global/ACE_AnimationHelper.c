c202fb19240e09087c380cf24a79a379387c2d29
Kex <kexanone@gmail.com>
2025-03-30 18:29:18 +0200

    Core - Implement common approach to animation helpers (#165)
    * Implement common approach to animation helper and carrying them
    
    * Move animation ID definition to config
    
    * Fix is carrying check
    
    * Prevent double execution of termination
    
    * Add attribute for get out type when life state has changed
    
    * Limit number of attempts for move out and get out
    
    * Implement "get in" method
    
    * Use new get in method
    
    * Pass manager ID for getting the correct slot

---
+++ addons/core/scripts/Game/ACE_Core/Global/ACE_AnimationHelper.c
@@ -0,0 +1,56 @@
+//------------------------------------------------------------------------------------------------
+class ACE_AnimationTools
+{
+	protected static ref ACE_AnimationHelpersConfig s_AnimationHelpersConfig;
+	protected static const ResourceName ANIMATION_HELPERS_CONFIG_NAME = "{49EB6E775F6E8519}Configs/ACE/ACE_AnimationHelpers.conf";
+	
+	//------------------------------------------------------------------------------------------------
+	protected static void Init()
+	{
+		s_AnimationHelpersConfig = SCR_ConfigHelperT<ACE_AnimationHelpersConfig>.GetConfigObject(ANIMATION_HELPERS_CONFIG_NAME);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	static ACE_AnimationHelperCompartment AnimateWithHelperCompartment(ACE_EAnimationHelperID helperID, notnull SCR_ChimeraCharacter performer)
+	{
+		vector transform[4];
+		performer.GetWorldTransform(transform);
+		return AnimateWithHelperCompartment(helperID, performer, transform);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	static ACE_AnimationHelperCompartment AnimateWithHelperCompartment(ACE_EAnimationHelperID helperID, notnull SCR_ChimeraCharacter performer, vector transform[4])
+	{
+		if (!s_AnimationHelpersConfig)
+			Init();
+		
+		Resource res = Resource.Load(s_AnimationHelpersConfig.GetPrefabName(helperID));
+		if (!res.IsValid())
+			return null;
+		
+		EntitySpawnParams params = new EntitySpawnParams();
+		params.TransformMode = ETransformMode.WORLD;
+		params.Transform = transform;
+		
+		ACE_AnimationHelperCompartment helper = ACE_AnimationHelperCompartment.Cast(GetGame().SpawnEntityPrefab(res, null, params));
+		if (!helper)
+			return null;
+		
+		helper.Init(performer);
+		return helper;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	static ACE_AnimationHelperCompartment GetHelperCompartment(notnull IEntity performer)
+	{
+		return ACE_AnimationHelperCompartment.Cast(performer.GetParent());;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	static void TerminateHelperCompartment(notnull IEntity performer, EGetOutType getOutType = EGetOutType.ANIMATED)
+	{
+		ACE_AnimationHelperCompartment helper = GetHelperCompartment(performer);
+		if (helper)
+			helper.Terminate(getOutType);
+	}
+}

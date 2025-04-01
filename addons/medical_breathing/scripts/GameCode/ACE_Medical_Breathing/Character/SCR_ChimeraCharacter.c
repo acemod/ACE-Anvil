3d118d4256777df99e5f7fbad77c4ffb89148407
Kex <kexanone@gmail.com>
2025-04-01 18:09:22 +0200

    Fix race condition for definition of medical components

--- addons/medical_breathing/scripts/GameCode/ACE_Medical_Breathing/Character/SCR_ChimeraCharacter.c
+++ addons/medical_breathing/scripts/GameCode/ACE_Medical_Breathing/Character/SCR_ChimeraCharacter.c
@@ -5,15 +5,11 @@ modded class SCR_ChimeraCharacter : ChimeraCharacter
 	protected ACE_Medical_RespiratoryComponent m_pACE_Medical_RespiratoryComponent;
 	
 	//------------------------------------------------------------------------------------------------
-	override void EOnInit(IEntity owner)
-	{
-		super.EOnInit(owner);
-		m_pACE_Medical_RespiratoryComponent = ACE_Medical_RespiratoryComponent.Cast(FindComponent(ACE_Medical_RespiratoryComponent));
-	}
-	
-	//------------------------------------------------------------------------------------------------
 	ACE_Medical_RespiratoryComponent ACE_Medical_GetRespiratoryComponent()
 	{
+		if (!m_pACE_Medical_RespiratoryComponent)
+			m_pACE_Medical_RespiratoryComponent = ACE_Medical_RespiratoryComponent.Cast(FindComponent(ACE_Medical_RespiratoryComponent));
+		
 		return m_pACE_Medical_RespiratoryComponent;
 	}
 }
--- addons/medical_prototypes/scripts/GameCode/ACE_Medical_Prototypes/Character/SCR_ChimeraCharacter.c
+++ addons/medical_prototypes/scripts/GameCode/ACE_Medical_Prototypes/Character/SCR_ChimeraCharacter.c
@@ -6,22 +6,20 @@ modded class SCR_ChimeraCharacter : ChimeraCharacter
 	protected ACE_Medical_MedicationComponent m_pACE_Medical_MedicationComponent;
 	
 	//------------------------------------------------------------------------------------------------
-	override void EOnInit(IEntity owner)
-	{
-		super.EOnInit(owner);
-		m_pACE_Medical_CardiovascularComponent = ACE_Medical_CardiovascularComponent.Cast(FindComponent(ACE_Medical_CardiovascularComponent));
-		m_pACE_Medical_MedicationComponent =  ACE_Medical_MedicationComponent.Cast(FindComponent(ACE_Medical_MedicationComponent));
-	}
-	
-	//------------------------------------------------------------------------------------------------
 	ACE_Medical_CardiovascularComponent ACE_Medical_GetCardiovascularComponent()
 	{
+		if (!m_pACE_Medical_CardiovascularComponent)
+			m_pACE_Medical_CardiovascularComponent = ACE_Medical_CardiovascularComponent.Cast(FindComponent(ACE_Medical_CardiovascularComponent));
+		
 		return m_pACE_Medical_CardiovascularComponent;
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	ACE_Medical_MedicationComponent ACE_Medical_GetMedicationComponent()
 	{
+		if (!m_pACE_Medical_MedicationComponent)
+			m_pACE_Medical_MedicationComponent =  ACE_Medical_MedicationComponent.Cast(FindComponent(ACE_Medical_MedicationComponent));
+		
 		return m_pACE_Medical_MedicationComponent;
 	}
 }

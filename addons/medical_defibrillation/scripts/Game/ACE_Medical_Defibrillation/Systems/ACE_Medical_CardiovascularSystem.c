2f52d24cefd74250fafc20a612ee46d41ab5914e
Hypoxic <martini_a@outlook.com>
2025-04-10 21:48:54 -0600

    Update ACE_Medical_CardiovascularSystem.c

--- addons/medical_defibrillation/scripts/Game/ACE_Medical_Defibrillation/Systems/ACE_Medical_CardiovascularSystem.c
+++ addons/medical_defibrillation/scripts/Game/ACE_Medical_Defibrillation/Systems/ACE_Medical_CardiovascularSystem.c
@@ -15,6 +15,8 @@ modded class ACE_Medical_CardiovascularSystem
 			return;
 		
 		UpdateCardiacRhythm(component, damageManager, timeSlice);
+		UpdateShockCooldown(component, damageManager, timeSlice);
+		UpdateShockAmount(component, damageManager, timeSlice);
 	}
 	
 	//------------------------------------------------------------------------------------------------
@@ -31,6 +33,58 @@ modded class ACE_Medical_CardiovascularSystem
 		{
 			component.SetCardiacRhythm(ACE_Medical_ECardiacRhythm.Sinus);
 		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void UpdateShockCooldown(ACE_Medical_CardiovascularComponent component, SCR_CharacterDamageManagerComponent damageManager, float timeSlice)
+	{
+		float current = component.GetShockCooldown();
+		
+		if (current <= 0)
+			return;
+		
+		float newCooldown = current - (m_fMinEntityUpdateTimeoutMS / 1000);
+		
+		if (newCooldown <= 0)
+		{
+			component.SetShockCooldown(0, true);
+		}
+		else
+		{
+			component.SetShockCooldown(newCooldown);
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void UpdateShockAmount(ACE_Medical_CardiovascularComponent component, SCR_CharacterDamageManagerComponent damageManager, float timeSlice)
+	{
+		SCR_ChimeraCharacter ownerChar = SCR_ChimeraCharacter.Cast(component.GetOwner());
+		if (!ownerChar)
+			return;
+		
+		CharacterControllerComponent ownerController = ownerChar.GetCharacterController();
+		if (!ownerController)
+			return;
+		
+		if (!ownerController.IsUnconscious())
+		{
+			if (component.GetShocksDelivered() > 0)
+				component.ResetShocksDelivered();
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override bool ComputeCPRSuccess(ACE_Medical_CardiovascularComponent component, SCR_CharacterDamageManagerComponent damageManager)
+	{
+		SCR_CharacterBloodHitZone bloodHZ = damageManager.GetBloodHitZone();
+		
+		float minScale = bloodHZ.GetDamageStateThreshold(ACE_Medical_EBloodState.CLASS_4_HEMORRHAGE);
+		float maxScale = bloodHZ.GetDamageStateThreshold(ACE_Medical_EBloodState.CLASS_2_HEMORRHAGE);
+		float scale = Math.Clamp(bloodHZ.GetHealthScaled(), minScale, maxScale);
+		float chance = Math.Map(scale, minScale, maxScale, m_Settings.m_fCPRSuccessChanceMin, m_Settings.m_fCPRSuccessChanceMax);
+		float diceRoll = Math.RandomFloat01();
+		
+		return diceRoll < chance + (component.GetShockChanceIncrease() * component.GetShocksDelivered());
 	}
 
 #ifdef WORKBENCH
@@ -54,6 +108,7 @@ modded class ACE_Medical_CardiovascularSystem
 		DbgUI.Begin(string.Format("ACE_Medical_DefibrillationSystem (%1)", targetType), 0, 700);
 		DbgUI.Text(string.Format("Cardiac rhythm:                  %1", rhythmName));
 		DbgUI.Text(string.Format("# of Shocks:                  %1", component.GetShocksDelivered()));
+		DbgUI.Text(string.Format("Shock cooldown:                  %1", component.GetShockCooldown()));
 		DbgUI.End();
 	}
 #endif

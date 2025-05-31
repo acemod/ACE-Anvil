201bc3575c98db7521b2ef1043030ae0cc6888b7
Kex <kexanone@gmail.com>
2025-05-31 02:23:17 +0200

    Move vital hit zone and second chance handling from ACE_Medical_VitalHitZoneHelper to SCR_CharacterHitZone

--- addons/medical_prototypes/scripts/Game/ACE_Medical_Prototypes/HitZone/SCR_CharacterHitZone.c
+++ addons/medical_prototypes/scripts/Game/ACE_Medical_Prototypes/HitZone/SCR_CharacterHitZone.c
@@ -1,7 +1,18 @@
 //------------------------------------------------------------------------------------------------
 modded class SCR_CharacterHitZone : SCR_RegeneratingHitZone
 {
+	[Attribute(defvalue: "false", desc: "Whether the hit zone is vital and will kill the character when destroyed")]
+	protected bool m_bACE_Medical_IsVital;
+	
+	[Attribute(defvalue: "false", desc: "Whether second chance is enabled for this hit zone (only relveant when hit zone is vital)")]
+	protected bool m_bACE_Medical_SecondChanceEnabled;
+	
 	protected SCR_CharacterDamageManagerComponent m_pACE_Medical_DamageManager;
+	protected ACE_Medical_CardiovascularComponent m_pACE_Medical_CardiovascularComponent;
+	
+	// Deactivation time is shared across all hit zones. This ensures that other vital hit zones that get destroyed within this time are also protected
+	protected static const float ACE_MEDICAL_SECOND_CHANCE_DEACTIVATION_TIMEOUT_MS = 1000;
+	protected static const float ACE_MEDICAL_SECOND_CHANCE_RESCALED_HEALTH = 0.1;
 	
 	//-----------------------------------------------------------------------------------------------------------
 	//! Initialize member variables
@@ -9,10 +20,11 @@ modded class SCR_CharacterHitZone : SCR_RegeneratingHitZone
 	{
 		super.OnInit(pOwnerEntity, pManagerComponent);
 		m_pACE_Medical_DamageManager = SCR_CharacterDamageManagerComponent.Cast(pManagerComponent);
+		m_pACE_Medical_CardiovascularComponent = ACE_Medical_CardiovascularComponent.Cast(pOwnerEntity.FindComponent(ACE_Medical_CardiovascularComponent));
 	}
 	
 	//-----------------------------------------------------------------------------------------------------------
-	//! Add bleeding to physical hit zones that get destroyed
+	//! Add handling for vital hit zones
 	override void OnDamageStateChanged(EDamageState newState, EDamageState previousDamageState, bool isJIP)
 	{
 		super.OnDamageStateChanged(newState, previousDamageState, isJIP);
@@ -23,6 +35,25 @@ modded class SCR_CharacterHitZone : SCR_RegeneratingHitZone
 		if (newState != EDamageState.DESTROYED)
 			return;
 		
+		//! Add bleeding to physical hit zones that get destroyed
 		m_pACE_Medical_DamageManager.AddBleedingEffectOnHitZone(this);
+		
+		if (!m_bACE_Medical_IsVital)
+			return;
+		
+		bool hasLastSecondChanceExpired = System.GetTickCount(m_pACE_Medical_DamageManager.ACE_Medical_GetLastSecondChanceTickCount()) >= ACE_MEDICAL_SECOND_CHANCE_DEACTIVATION_TIMEOUT_MS;
+		if (!m_bACE_Medical_SecondChanceEnabled || (m_pACE_Medical_CardiovascularComponent.IsInCardiacArrest() && hasLastSecondChanceExpired))
+		{
+			m_pACE_Medical_DamageManager.Kill(m_pACE_Medical_DamageManager.GetInstigator());
+		}
+		// Handle second chance
+		else
+		{
+			SetHealthScaled(ACE_MEDICAL_SECOND_CHANCE_RESCALED_HEALTH);
+			m_pACE_Medical_CardiovascularComponent.SetVitalState(ACE_Medical_EVitalState.CARDIAC_ARREST);
+			
+			if (hasLastSecondChanceExpired)
+				m_pACE_Medical_DamageManager.ACE_Medical_OnSecondChanceTriggered(this);
+		}
 	}
 }

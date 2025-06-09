2361e9ee49795e81453a2c9dba8cbb9af617627c
Kex <kexanone@gmail.com>
2025-06-10 00:06:07 +0200

    Slider for m_fMinHealthScaledForEpinephrine

--- addons/medical_core/scripts/Game/@ACE_Medical_Core/Settings/ACE_Medical_SettingsConfig.c
+++ addons/medical_core/scripts/Game/@ACE_Medical_Core/Settings/ACE_Medical_SettingsConfig.c
@@ -5,6 +5,6 @@ class ACE_Medical_Settings : ACE_ModSettings
 	[Attribute(defvalue: "0.3", desc: "Default resilience regeneration scale. The default regeneration rate will be multiplied by this factor.")]
 	float m_fDefaultResilienceRegenScale;
 	
-	[Attribute(defvalue: "0.33", desc: "Can only apply epinephrine when patient has at least this health (ignored in advanced medication).")]
+	[Attribute(defvalue: "0.33", desc: "Can only apply epinephrine when patient has at least this health (ignored in advanced medication).", uiwidget: UIWidgets.Slider, params: "0 1")]
 	float m_fMinHealthScaledForEpinephrine;
 }

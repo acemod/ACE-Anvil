43330a261df80703df758b3ac1a60b333bb227f4
Kex <kexanone@gmail.com>
2025-06-10 00:07:18 +0200

    Method for compiling attribute param array of character hit zones

---
+++ addons/medical_core/scripts/Game/@ACE_Medical_Core/Global/SCR_ParamEnumArray.c
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------------------------
+modded class SCR_ParamEnumArray : ParamEnumArray
+{
+	//------------------------------------------------------------------------------------------------
+	static ParamEnumArray ACE_Medical_FromCharacterBaseHitZones()
+	{
+		IEntityComponentSource componentSource = SCR_BaseContainerTools.FindComponentSource(Resource.Load("{37578B1666981FCE}Prefabs/Characters/Core/Character_Base.et"), SCR_DamageManagerComponent);
+		if (!componentSource)
+			return null;
+		
+		BaseContainerList list = componentSource.GetObjectArray("Additional hit zones");
+		
+		ParamEnumArray params = new ParamEnumArray();
+		int cnt = list.Count();
+		int val;
+	
+		for (int i = 0; i < cnt; ++i)
+		{
+			string hitZoneName = list.Get(i).GetName();
+			params.Insert(new ParamEnum(hitZoneName, hitZoneName));
+		}
+		
+		return params;
+	}
+}

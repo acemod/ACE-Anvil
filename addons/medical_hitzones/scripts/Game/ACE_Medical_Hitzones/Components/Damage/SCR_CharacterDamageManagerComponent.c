ed7dcd45f3d38060178118368cb3773a1def384e
Kex <kexanone@gmail.com>
2025-06-14 00:28:14 +0200

    Implement massive bleeding effect and apply it to neck

--- addons/medical_hitzones/scripts/Game/ACE_Medical_Hitzones/Components/Damage/SCR_CharacterDamageManagerComponent.c
+++ addons/medical_hitzones/scripts/Game/ACE_Medical_Hitzones/Components/Damage/SCR_CharacterDamageManagerComponent.c
@@ -1,4 +1,22 @@
 //------------------------------------------------------------------------------------------------
+modded class SCR_CharacterDamageManagerComponentClass : SCR_DamageManagerComponentClass
+{
+	[Attribute(defvalue: "500", desc: "Particle birth rate for massive bleedings")]
+	protected int m_iACE_Medical_MassiveBleedingParticleBirthRate;
+	
+	[Attribute(defvalue: "1", desc: "Particle velocity for massive bleedings")]
+	protected float m_fACE_Medical_MassiveBleedingParticleVelocity;
+	
+	//------------------------------------------------------------------------------------------------
+	//! Reconfigures particles params for massive bleeding
+	void ACE_Medical_UpgradeToMassiveBleeding(Particles particles)
+	{
+		particles.SetParam(-1, EmitterParam.BIRTH_RATE, m_iACE_Medical_MassiveBleedingParticleBirthRate);
+		particles.SetParam(-1, EmitterParam.VELOCITY, m_fACE_Medical_MassiveBleedingParticleVelocity);
+	}
+}
+
+//------------------------------------------------------------------------------------------------
 modded class SCR_CharacterDamageManagerComponent : SCR_DamageManagerComponent
 {
 	//-----------------------------------------------------------------------------------------------------------
@@ -20,6 +38,20 @@ modded class SCR_CharacterDamageManagerComponent : SCR_DamageManagerComponent
 			SCR_CharacterHitZone charHitZone = SCR_CharacterHitZone.Cast(hitZone);
 			if (charHitZone)
 				charHitZone.ACE_Medical_InitOrganHitZones(this);
+		}
+	}
+	
+	//-----------------------------------------------------------------------------------------------------------
+	//! Handle massive bleeding effects
+	override void CreateBleedingParticleEffect(notnull HitZone hitZone, int colliderDescriptorIndex)
+	{
+		super.CreateBleedingParticleEffect(hitZone, colliderDescriptorIndex);
+		
+		if (ACE_Medical_NeckHitZone.Cast(hitZone) && m_mBleedingParticles && m_mBleedingParticles.Contains(hitZone))
+		{
+			SCR_CharacterDamageManagerComponentClass componentData = SCR_CharacterDamageManagerComponentClass.Cast(GetComponentData(GetOwner()));
+			if (componentData)
+				componentData.ACE_Medical_UpgradeToMassiveBleeding(m_mBleedingParticles[hitZone].GetParticles());
 		}
 	}
 	

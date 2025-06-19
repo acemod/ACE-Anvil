6cc440bc288a66b16f83060f745070f2c417bb28
Kex <kexanone@gmail.com>
2025-06-20 00:15:28 +0200

    Add ranges to float settings

--- addons/medical_core/scripts/Game/ACE_Medical_Bleeding/Settings/ACE_Medical_Core_Settings.c
+++ addons/medical_core/scripts/Game/ACE_Medical_Bleeding/Settings/ACE_Medical_Core_Settings.c
@@ -5,12 +5,12 @@ modded class ACE_Medical_Core_Settings : ACE_ModSettings
 	[Attribute(defvalue: "true", desc: "Whether players can bleed out")]
 	bool m_bBleedOutForPlayersEnabled;
 	
-	[Attribute(defvalue: "1", desc: "Character bleeding rate multiplier")]
+	[Attribute(defvalue: "1", desc: "Character bleeding rate multiplier", params: "0 inf")]
 	float m_fBleedingRateScale;
 	
-	[Attribute(defvalue: "-1", desc: "Maximum possibe bleeding rate in ml/s (Negative value means unlimited)")]
+	[Attribute(defvalue: "-1", desc: "Maximum possibe bleeding rate in ml/s (Negative value means unlimited)", params: "-1 inf")]
 	float m_fMaxTotalBleedingRate;
 	
-	[Attribute(defvalue: "1", desc: "Character blood regeneration rate multiplier")]
+	[Attribute(defvalue: "1", desc: "Character blood regeneration rate multiplier", params: "0 inf")]
 	float m_fBloodRegenScale;
 }

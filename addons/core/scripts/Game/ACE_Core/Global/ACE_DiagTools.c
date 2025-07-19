a80870ea7b2bb121f84391591d8c9aa680db591a
Kex <kexanone@gmail.com>
2025-07-19 17:17:20 +0200

    Add diag method for selecting target character

---
+++ addons/core/scripts/Game/ACE_Core/Global/ACE_DiagTools.c
@@ -0,0 +1,31 @@
+#ifdef WORKBENCH
+//------------------------------------------------------------------------------------------------
+class ACE_DiagTools
+{
+	//------------------------------------------------------------------------------------------------
+	//! Get target for diag menu and string for printing which target it is
+	//! Return true if a target was found
+	static bool GetDiagTargetCharacter(out IEntity target, out string targetType)
+	{
+		CameraBase camera = GetGame().GetCameraManager().CurrentCamera();
+		if (!camera)
+			return false;
+		
+		target = SCR_ChimeraCharacter.Cast(camera.GetCursorTarget());
+		if (target)
+		{
+			targetType = "Target";
+			return true;
+		}
+		
+		target = SCR_ChimeraCharacter.Cast(SCR_PlayerController.GetLocalControlledEntity());
+		if (target)
+		{
+			targetType = "Self";
+			return true;
+		}
+		
+		return false;
+	}
+}
+#endif

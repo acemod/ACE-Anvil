c2431389bbb66facf62009e887acbc480285ab95
Kex <kexanone@gmail.com>
2025-08-26 18:01:08 +0200

    Implement muzzle dispersion temperature curve

--- addons/overheating/scripts/Game/ACE_Overheating/Components/Weapons/ACE_Overheating_BarrelComponent.c
+++ addons/overheating/scripts/Game/ACE_Overheating/Components/Weapons/ACE_Overheating_BarrelComponent.c
@@ -25,6 +25,20 @@ class ACE_Overheating_BarrelComponentClass : ScriptComponentClass
 	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Probability to jam vs temperature [K]", params: "1300 0.1 0 0")]
 	protected ref Curve m_cJamChanceTemperatureCurve;
 	
+	// Additional temperature-dependent muzzle dispersion factor f
+	// -----------------------------------------------------------
+	// Base dispersion cone diameter gets multiplied by (1 + f)
+	//
+	// Formula: f = tan(a + da(T)) / tan(a) - 1
+	// First-order Taylor approximation for small angles: f ≈ da(T) / a
+	// where a is base dispersion angle and da(T) is the additional temperature-dependent dispersion
+	//
+	// Using Reforger's M16A4 base dispersion: Cone height = 400 m; Cone diameter = 0.4 m => a ≈ 0.5 mrad
+	// and ACE3 additional dispersion: 0 mils at 0 °C, 0.5 mils at 333°C, 2.2 mils at 666 °C, 5 mils at 1000 °C
+	// => Additional dispersion factors: 0.0 at 0 °C, 1.0 at 333°C, 4.4 at 666 °C, 10.0 at 1000 °C
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Additional muzzle dispersion factor vs temperature [K]", params: "1300 30 0 0")]
+	protected ref Curve m_cMuzzleDispersionFactorTemperatureCurve;
+	
 	protected bool m_bInitDone = false;
 	protected float m_fHeatPerShot;
 	protected float m_fBarrelHeatCapacity;
@@ -168,6 +182,12 @@ class ACE_Overheating_BarrelComponentClass : ScriptComponentClass
 	float ComputeJamChance(float temperature)
 	{
 		return Math3D.Curve(ECurveType.CurveProperty2D, temperature, m_cJamChanceTemperatureCurve)[1];
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	float ComputeAdditionalMuzzleDispersionFactor(float temperature)
+	{
+		return Math3D.Curve(ECurveType.CurveProperty2D, temperature, m_cMuzzleDispersionFactorTemperatureCurve)[1];
 	}
 }
 

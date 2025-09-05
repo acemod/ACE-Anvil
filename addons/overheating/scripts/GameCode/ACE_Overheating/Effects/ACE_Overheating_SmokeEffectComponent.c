b3f10a672b1a64e803b05bd184b18dda7fafc680
Kex <kexanone@gmail.com>
2025-09-05 22:52:56 +0200

    Add new attributes to smoke component

--- addons/overheating/scripts/GameCode/ACE_Overheating/Effects/ACE_Overheating_SmokeEffectComponent.c
+++ addons/overheating/scripts/GameCode/ACE_Overheating/Effects/ACE_Overheating_SmokeEffectComponent.c
@@ -6,6 +6,41 @@ class ACE_Overheating_SmokeEffectComponentClass : MuzzleEffectComponentClass
 //------------------------------------------------------------------------------------------------
 class ACE_Overheating_SmokeEffectComponent : MuzzleEffectComponent
 {
+	
+	[Attribute(defvalue: "{5EF5A6DBC1EA1B1D}Particles/Weapon/ACE_Overheating_MuzzleSmoke.ptc", uiwidget: UIWidgets.ResourceNamePicker)]
+	protected ResourceName m_sMuzzleSmokeEffectName;
+	
+	[Attribute(defvalue: "{5EF5A6DBC1EA1B1D}Particles/Weapon/ACE_Overheating_MuzzleSmoke.ptc", uiwidget: UIWidgets.ResourceNamePicker)]
+	protected ResourceName m_sBarrelSurfaceSmokeEffectName;
+	
+	[Attribute()]
+	protected ref array<string> m_aTemperatureDependentEmitterNames;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Birth rate vs temperature [K]", params: "1300 100 0 0")]
+	protected ref Curve m_cBirthRateTemperatureCurve;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Birth rate RND vs temperature [K]", params: "1300 100 0 0")]
+	protected ref Curve m_cBirthRateRndTemperatureCurve;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Particle lifetime multiplier vs temperature [K]", params: "1300 4 0 0")]
+	protected ref Curve m_cLifetimeMultiplierMultiplierTemperatureCurve;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Particle lifetime RND vs temperature [K]", params: "1300 4 0 0")]
+	protected ref Curve m_cLifetimeRndMultiplierTemperatureCurve;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Particle size multiplier vs temperature [K]", params: "1300 1 0 0")]
+	protected ref Curve m_cSizeMultiplierTemperatureCurve;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Particle size RND vs temperature [K]", params: "1300 1 0 0")]
+	protected ref Curve m_cSizeRndTemperatureCurve;
+
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Particle velocity vs temperature [K]", params: "1300 3 0 0")]
+	protected ref Curve m_cVelocityMultiplierTemperatureCurve;
+	
+	[Attribute(uiwidget: UIWidgets.GraphDialog, desc: "Particle velocity RND vs temperature [K]", params: "1300 3 0 0")]
+	protected ref Curve m_cVelocityRndTemperatureCurve;
+	
 	protected ACE_Overheating_BarrelComponent m_pBarrel;
 	protected static ACE_Overheating_Settings s_pSettings;
 		
@@ -32,6 +67,9 @@ class ACE_Overheating_SmokeEffectComponent : MuzzleEffectComponent
 		ParticleEffectEntitySpawnParams spawnParams = new ParticleEffectEntitySpawnParams();
 		spawnParams.Parent = weapon;
 		spawnParams.PivotID = weaponAnim.GetBoneIndex("barrel_muzzle");
-		ParticleEffectEntity effect = ParticleEffectEntity.SpawnParticleEffect("{5EF5A6DBC1EA1B1D}Particles/Weapon/ACE_Overheating_MuzzleSmoke.ptc", spawnParams);
+		ParticleEffectEntity muzzleEffect = ParticleEffectEntity.SpawnParticleEffect(m_sMuzzleSmokeEffectName, spawnParams);
+		
+		//spawnParams.Transform[3] - 
+		ParticleEffectEntity barrelEffect = ParticleEffectEntity.SpawnParticleEffect(m_sBarrelSurfaceSmokeEffectName, spawnParams);
 	}
 }

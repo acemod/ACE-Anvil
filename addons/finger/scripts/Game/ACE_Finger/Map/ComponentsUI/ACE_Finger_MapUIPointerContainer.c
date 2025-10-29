c92d35fdbb6d2500f7de166abaca517212edfd87
Kex <kexanone@gmail.com>
2025-10-29 22:37:34 +0100

    Update addons/finger/scripts/Game/ACE_Finger/Map/ComponentsUI/ACE_Finger_MapUIPointerContainer.c
    Co-authored-by: Filip Maciejewski <veteran29@users.noreply.github.com>

--- addons/finger/scripts/Game/ACE_Finger/Map/ComponentsUI/ACE_Finger_MapUIPointerContainer.c
+++ addons/finger/scripts/Game/ACE_Finger/Map/ComponentsUI/ACE_Finger_MapUIPointerContainer.c
@@ -117,9 +117,9 @@ class ACE_Finger_MapUIPointerContainer : SCR_MapUIBaseComponent
 		ptr.SetPos(Vector(xWorld, 0, zWorld));
 		ptr.ToggleActive(true);
 		
-		SCR_MapCursorModule curstorModule = SCR_MapCursorModule.Cast(m_MapEntity.GetMapModule(SCR_MapCursorModule));
-		if (curstorModule)
-			curstorModule.ACE_Finger_HandlePointing(true);
+		SCR_MapCursorModule cursorModule = SCR_MapCursorModule.Cast(m_MapEntity.GetMapModule(SCR_MapCursorModule));
+		if (cursorModule)
+			cursorModule.ACE_Finger_HandlePointing(true);
 	}
 	
 	//------------------------------------------------------------------------------------------------

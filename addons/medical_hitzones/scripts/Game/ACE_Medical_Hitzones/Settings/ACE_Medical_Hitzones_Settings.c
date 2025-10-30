8e3bd2fea85037174bd6f6f03bc63567f10e2e53
Kex <kexanone@gmail.com>
2025-10-30 15:35:45 +0100

    Medical - Add Hitzones addon (#230)
    * Add addon
    
    * Implement option to mark character hit zone as vital
    
    * Disable health hit zone and set vital hit zones
    
    * Implement organ hit zones
    
    * Draw debug shapes for organ hit zones
    
    * Add heart hit zone
    
    * Add neck hit zone
    
    * Implement massive bleeding effect and apply it to neck
    
    * Remove bleeding effect when character dies
    
    * Implement femoral arteries
    
    * Create separate settings
    
    * Add settings to config
    
    * Update dependency GUID
    
    * Add to AiO
    
    * Fix crash caused by Kill method
    
    * Add diag menu setting for drawing organ hit zones
    
    * Add custom diag method for organ hit zones
    
    * Fix VME on dedicated
    
    * Return lowest health

---
+++ addons/medical_hitzones/scripts/Game/ACE_Medical_Hitzones/Settings/ACE_Medical_Hitzones_Settings.c
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------------------------
+[BaseContainerProps()]
+class ACE_Medical_Hitzones_Settings : ACE_ModSettings
+{
+	[Attribute(defvalue: "true", desc: "Whether organ hit zones should be enabled")]
+	bool m_bOrganHitZonesEnabled;
+	
+	[Attribute(defvalue: "0", desc: "Probability for second chance for killing blows to the heart", uiwidget: UIWidgets.Slider, params: "0 1")]
+	float m_fSecondChanceOnHeart;
+}

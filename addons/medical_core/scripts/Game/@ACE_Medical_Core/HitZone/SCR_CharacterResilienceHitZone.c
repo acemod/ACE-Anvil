183f05e30ddcce82e27d2844cb59fcd901ab84de
fedoraman737 <monn45888@gmail.com>
2025-11-08 21:22:04 -0600

    Ignore scaling on HEALING damage type

--- addons/medical_core/scripts/Game/@ACE_Medical_Core/HitZone/SCR_CharacterResilienceHitZone.c
+++ addons/medical_core/scripts/Game/@ACE_Medical_Core/HitZone/SCR_CharacterResilienceHitZone.c
@@ -2,7 +2,7 @@
 modded class SCR_CharacterResilienceHitZone : SCR_RegeneratingHitZone
 {
 	//------------------------------------------------------------------------------------------------
-	//! Multiply regen by the regen scale and all other damage by the damage scale
+	//! Multiply regen by the regen scale and incoming damage by the damage scale
 	override float ComputeEffectiveDamage(notnull BaseDamageContext damageContext, bool isDOT)
 	{
 		float damage = super.ComputeEffectiveDamage(damageContext, isDOT);
@@ -12,6 +12,9 @@ modded class SCR_CharacterResilienceHitZone : SCR_RegeneratingHitZone
 
 		SCR_CharacterDamageManagerComponent damageManager = SCR_CharacterDamageManagerComponent.Cast(GetHitZoneContainer());
 		if (!damageManager)
+			return damage;
+
+		if (damageContext.damageType == EDamageType.HEALING)
 			return damage;
 
 		if (damageContext.damageType == EDamageType.REGENERATION)

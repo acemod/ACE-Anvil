aca9ad1111aa2f46cca6ff33ff2da9e3e460467b
fedoraman737 <150987667+fedoraman737@users.noreply.github.com>
2025-11-08 18:39:29 -0600

    Update description to remove redundancy
    Co-authored-by: Kex <kexanone@gmail.com>

--- addons/medical_core/scripts/Game/@ACE_Medical_Core/Settings/ACE_Medical_Core_Settings.c
+++ addons/medical_core/scripts/Game/@ACE_Medical_Core/Settings/ACE_Medical_Core_Settings.c
@@ -5,7 +5,7 @@ class ACE_Medical_Core_Settings : ACE_ModSettings
 	[Attribute(defvalue: "0.3", desc: "Default resilience regeneration scale. The default regeneration rate (5 HP/s) will be multiplied by this factor.", params: "0 inf")]
 	float m_fDefaultResilienceRegenScale;
 
-	[Attribute(defvalue: "1.0", desc: "Resilience damage scale. All incoming damage (except regeneration) to the resilience hitzone will be multiplied by this factor. Lower values make characters more resilient.", params: "0 inf")]
+	[Attribute(defvalue: "1.0", desc: "Resilience damage scale. All incoming damage to the resilience hitzone will be multiplied by this factor. Lower values make characters less likely to fall unconscious.", params: "0 inf")]
 	float m_fResilienceDamageScale;
 
 	[Attribute(defvalue: "0.33", desc: "Can only apply epinephrine when patient has at least this health (ignored in advanced medication).", uiwidget: UIWidgets.Slider, params: "0 1")]

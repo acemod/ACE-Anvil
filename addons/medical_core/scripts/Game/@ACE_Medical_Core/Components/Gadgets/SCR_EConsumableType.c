b451d174f6e8dbc96cce88e87dc657b4f15b5942
Kex <kexanone@gmail.com>
2025-11-12 16:49:07 +0100

    Add splint effect

--- addons/medical_core/scripts/Game/@ACE_Medical_Core/Components/Gadgets/SCR_EConsumableType.c
+++ addons/medical_core/scripts/Game/@ACE_Medical_Core/Components/Gadgets/SCR_EConsumableType.c
@@ -2,5 +2,6 @@
 //! Type of consumable gadget
 modded enum SCR_EConsumableType
 {
-	ACE_MEDICAL_EPINEPHRINE
+	ACE_MEDICAL_EPINEPHRINE,
+	ACE_MEDICAL_SPLINT
 }
--- addons/medical_core/scripts/Game/@ACE_Medical_Core/Damage/DamageEffects/CharacterDamageEffects/ACE_Medical_SplintDamageEffect.c
+++ addons/medical_core/scripts/Game/@ACE_Medical_Core/Damage/DamageEffects/CharacterDamageEffects/ACE_Medical_SplintDamageEffect.c
@@ -1,6 +1,38 @@
 //------------------------------------------------------------------------------------------------
 class ACE_Medical_SplintDamageEffect: SCR_DotDamageEffect
 {
+	protected ref array<HitZone> m_aAffectedHitZones = {};
+	
+	//------------------------------------------------------------------------------------------------
+	override void OnEffectAdded(SCR_ExtendedDamageManagerComponent dmgManager)
+	{
+		super.OnEffectAdded(dmgManager);
+		
+		SCR_CharacterDamageManagerComponent charDamageManager = SCR_CharacterDamageManagerComponent.Cast(dmgManager);
+		if (!charDamageManager)
+			return;
+		
+		SCR_CharacterHitZone charHitZone = SCR_CharacterHitZone.Cast(GetAffectedHitZone());
+		if (!charHitZone)
+			return;
+		
+		charDamageManager.GetHitZonesOfGroup(charHitZone.GetHitZoneGroup(), m_aAffectedHitZones);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected override void EOnFrame(float timeSlice, SCR_ExtendedDamageManagerComponent dmgManager)
+	{
+		float accurateTimeSlice = GetAccurateTimeSlice(timeSlice);
+		float damage = GetDPS() * accurateTimeSlice;
+		DotDamageEffectTimerToken token = UpdateTimer(accurateTimeSlice, dmgManager);
+		
+		foreach(HitZone hitZone : m_aAffectedHitZones)
+		{
+			if (hitZone.GetHealthScaled() < 0.667)
+				DealCustomDot(hitZone, damage, token, dmgManager);
+		}
+	}
+	
 	//------------------------------------------------------------------------------------------------
 	override EDamageType GetDefaultDamageType()
 	{

55ce0558468ed27f76b81097f979f8a52c1bbd7f
Kex <kexanone@gmail.com>
2025-11-16 03:36:06 +0100

    Change entity to world controller

--- addons/finger/scripts/Game/ACE_Finger/Systems/ACE_Finger_MapPointer.c
+++ addons/finger/scripts/Game/ACE_Finger/Systems/ACE_Finger_MapPointer.c
@@ -1,17 +1,11 @@
 //------------------------------------------------------------------------------------------------
-class ACE_Finger_MapPointerClass : GenericEntityClass
-{
-}
-
-//------------------------------------------------------------------------------------------------
-//! TO DO: Replace with public WorldController when new systems are available
-class ACE_Finger_MapPointer : GenericEntity
+class ACE_Finger_MapPointer : WorldController
 {
 	[RplProp(), Attribute(defvalue: "10", desc: "Range of the pointer in meters. Only players in range will see it. Anyone can see it if negative.")]
 	protected float m_fPointerRangeM;
 	
-	[RplProp(onRplName: "OnOwnerSet")]
-	protected int m_iOwnerPlayerID;
+	[RplProp()]
+	protected int m_iOwnerPlayerId;
 	
 	[RplProp(onRplName: "OnToggleActive")]
 	protected bool m_bState;
@@ -19,31 +13,52 @@ class ACE_Finger_MapPointer : GenericEntity
 	[RplProp()]
 	protected vector m_vPos;
 	
-	protected static ACE_Finger_MapPointer s_pLocalInstance;
+	protected static const int ATTEMPT_TIMEOUT_MS = 500;
 	
 	//------------------------------------------------------------------------------------------------
 	static ACE_Finger_MapPointer GetLocalInstance()
 	{
-		return s_pLocalInstance;
+		ChimeraWorld world = GetGame().GetWorld();
+		return ACE_Finger_MapPointer.Cast(world.GetSystems().FindMyController(ACE_Finger_MapPointer));
+	}
+	
+	override static void InitInfo(WorldControllerInfo outInfo)
+	{
+		outInfo.SetPublic(true);
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	void InitServer(int ownerPlayerID)
+	override protected void OnAuthorityReady()
 	{
 		ACE_Finger_Settings settings = ACE_SettingsHelperT<ACE_Finger_Settings>.GetModSettings();
 		if (settings)
 			m_fPointerRangeM = settings.m_fMapPointingRangeM;
 		
-		m_iOwnerPlayerID = ownerPlayerID;
-		OnOwnerSet();
 		Replication.BumpMe();
+		Rpc(RpcAsk_InitOwnerPlayerIdOwner);
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	protected void OnOwnerSet()
+	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
+	protected void RpcAsk_InitOwnerPlayerIdOwner()
 	{
-		if (m_iOwnerPlayerID == SCR_PlayerController.GetLocalPlayerId())
-			s_pLocalInstance = this;
+		int playerId = SCR_PlayerController.GetLocalPlayerId();
+		if (playerId <= 0)
+		{
+			// Try later if player controller wasn't ready yet
+			GetGame().GetCallqueue().CallLater(RpcAsk_InitOwnerPlayerIdOwner, false, ATTEMPT_TIMEOUT_MS);
+			return;
+		}
+		
+		Rpc(RpcDo_InitOwnerPlayerIdServer, playerId);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	protected void RpcDo_InitOwnerPlayerIdServer(int playerId)
+	{
+		m_iOwnerPlayerId = playerId;
+		Replication.BumpMe();
 	}
 	
 	//------------------------------------------------------------------------------------------------
@@ -102,9 +117,10 @@ class ACE_Finger_MapPointer : GenericEntity
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	int GetOwnerPlayerID()
+	//! Temporary workaround until WorldController:GetOwnerPlayerId works
+	int ACE_GetOwnerPlayerId()
 	{
-		return m_iOwnerPlayerID;
+		return m_iOwnerPlayerId;
 	}
 	
 	//------------------------------------------------------------------------------------------------

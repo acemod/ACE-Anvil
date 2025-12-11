527205ed89057e5f009712147c04af8980119308
Kex <kexanone@gmail.com>
2025-12-11 23:29:31 +0100

    Implement consumable and damage effects

---
+++ addons/medical_breathing/scripts/Game/ACE_Medical_Breathing/Damage/DamageEffects/CharacterDamageEffects/ACE_Medical_NCDKitDamageEffect.c
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------------------------
+class ACE_Medical_NCDKitDamageEffect : SCR_InstantDamageEffect
+{
+	//------------------------------------------------------------------------------------------------
+	protected override void HandleConsequences(SCR_ExtendedDamageManagerComponent dmgManager, DamageEffectEvaluator evaluator)
+	{
+		super.HandleConsequences(dmgManager, evaluator);
+		evaluator.HandleEffectConsequences(this, dmgManager);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void OnEffectAdded(SCR_ExtendedDamageManagerComponent dmgManager)
+	{
+		IEntity target = dmgManager.GetOwner();
+		if (!target)
+			return;
+		
+		ACE_Medical_VitalsComponent vitalsComponent = ACE_Medical_VitalsComponent.Cast(target.FindComponent(ACE_Medical_VitalsComponent));
+		if (vitalsComponent)
+			vitalsComponent.SetHasTensionPneumothorax(false);
+	}
+}

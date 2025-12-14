0204d30e04ab61fbe9757bd893cd5ef1d6d6a441
Kex <kexanone@gmail.com>
2025-12-14 18:01:14 +0100

    Refactor SCR_InspectCasualtyWidget

--- addons/medical_circulation/scripts/Game/ACE_Medical_Medication/Components/ACE_Medical_MedicationComponent.c
+++ addons/medical_circulation/scripts/Game/ACE_Medical_Medication/Components/ACE_Medical_MedicationComponent.c
@@ -87,6 +87,22 @@ class ACE_Medical_MedicationComponent : ScriptComponent
 	}
 	
 	//------------------------------------------------------------------------------------------------
+	string GetInfoText()
+	{
+		array<string> medicationTexts = {};
+		medicationTexts.Reserve(m_aLogMessageTimes.Count());
+		
+		for (int i = m_aLogMessageTimes.Count() - 1; i >= 0; i--)
+		{
+			int hours = Math.Floor(m_aLogMessageTimes[i]);
+			int minutes = Math.Round(60 * (m_aLogMessageTimes[i] - hours));
+			medicationTexts.Insert(string.Format("%1:%2 %3 (%4)", hours, minutes, m_aLogMessages[i], m_aLogMessageAuthors[i]));
+		}
+		
+		return SCR_StringHelper.Join("\n", medicationTexts);
+	}
+	
+	//------------------------------------------------------------------------------------------------
 	//! Clears the body of all drugs and effects
 	void Clear()
 	{

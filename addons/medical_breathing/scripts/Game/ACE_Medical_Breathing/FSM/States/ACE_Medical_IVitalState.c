443cf46ae2ee9313ab28478c4dca690a4b5bd61e
ArwynFr <10365998+ArwynFr@users.noreply.github.com>
2026-03-06 19:52:15 +0100

    fix: division by zero depending on pneumothorax state

--- addons/medical_breathing/scripts/Game/ACE_Medical_Breathing/FSM/States/ACE_Medical_IVitalState.c
+++ addons/medical_breathing/scripts/Game/ACE_Medical_Breathing/FSM/States/ACE_Medical_IVitalState.c
@@ -62,7 +62,11 @@ modded class ACE_Medical_IVitalState : ACE_FSM_IState<ACE_Medical_CharacterConte
 	protected void UpdatePerfusion(ACE_Medical_CharacterContext context, float timeSlice)
 	{
 		float alveolarOxygenSaturation = ACE_Math.Hill(Math.Pow(context.m_pVitals.GetPalvO2() / ACE_MaterialProperties.DEFAULT_KD_HEMOGLOBIN, ACE_MaterialProperties.HILL_COEFF_HEMOGLOBIN));
-		float perfusion = context.m_pVitals.GetCardiacOutput() / 60 * Math.Pow(1 - context.m_pVitals.GetPneumothoraxScale(), 2/3);
+		float pneumothoraxScale = context.m_pVitals.GetPneumothoraxScale();
+		if(pneumothoraxScale == 1)
+			return;
+
+		float perfusion = context.m_pVitals.GetCardiacOutput() / 60 * Math.Pow(1 - pneumothoraxScale, 2/3);
 		float maxFluxO2 = perfusion * context.m_pBloodHitZone.ACE_Medical_GetHemeConcentration() * alveolarOxygenSaturation;
 		
 		float rateScale = perfusion / s_fDefaultPerfusion;

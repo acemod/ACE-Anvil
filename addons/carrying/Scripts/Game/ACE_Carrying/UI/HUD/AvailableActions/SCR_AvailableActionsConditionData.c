f767d4ace9b4c84accc06136f1d499859f1ed3c1
Kex <kexanone@gmail.com>
2026-03-17 19:32:16 +0100

    Revert "run clang-format"
    This reverts commit a97a3e0888a63532044c20470d6f6628b0190320.

--- addons/carrying/Scripts/Game/ACE_Carrying/UI/HUD/AvailableActions/SCR_AvailableActionsConditionData.c
+++ addons/carrying/Scripts/Game/ACE_Carrying/UI/HUD/AvailableActions/SCR_AvailableActionsConditionData.c
@@ -1,20 +1,23 @@
-modded class SCR_AvailableActionsConditionData {
+modded class SCR_AvailableActionsConditionData
+{
     //! Is character carrying?
     protected bool m_ACE_Carrying_bIsCarrier;
 
     //------------------------------------------------------------------------------------------------
     //! Clears all variables by setting them to their default state
     //! Also invalidates this data
-    protected void ACE_Carrying_Clear() {
+    protected void ACE_Carrying_Clear()
+    {
         m_ACE_Carrying_bIsCarrier = false;
     }
 
     //------------------------------------------------------------------------------------------------
     //! Returns whether character is carrying
-    bool ACE_Carrying_GetIsCharacterCarrier() {
+    bool ACE_Carrying_GetIsCharacterCarrier()
+    {
         return m_ACE_Carrying_bIsCarrier;
     }
-
+    
     //------------------------------------------------------------------------------------------------
     // Fetch data
     //------------------------------------------------------------------------------------------------
@@ -22,9 +25,10 @@ modded class SCR_AvailableActionsConditionData {
     //------------------------------------------------------------------------------------------------
     //! Fetches data from the provided entity
     //! Sets the validity of the data which can be received via IsValid()
-    override void FetchData(IEntity controlledEntity, float timeSlice) {
+    override void FetchData(IEntity controlledEntity, float timeSlice)
+    {
         super.FetchData(controlledEntity, timeSlice);
-
+        
         // Invalidates and clears any data prior to following collection
         ACE_Carrying_Clear();
 

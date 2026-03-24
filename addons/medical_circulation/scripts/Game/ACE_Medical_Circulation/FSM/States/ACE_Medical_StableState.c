fb7d8e5b2f12ff5c3c11ffe48cbc6400865388f9
mazinskihenry <33608576+mazinskihenry@users.noreply.github.com>
2026-03-23 20:17:59 -0700

    Update ACE_Medical_StableState.c

--- addons/medical_circulation/scripts/Game/ACE_Medical_Circulation/FSM/States/ACE_Medical_StableState.c
+++ addons/medical_circulation/scripts/Game/ACE_Medical_Circulation/FSM/States/ACE_Medical_StableState.c
@@ -24,19 +24,21 @@ class ACE_Medical_StableState : ACE_Medical_IVitalState
 	//------------------------------------------------------------------------------------------------
 	override protected float ComputeHeartRate(ACE_Medical_CharacterContext context, float timeSlice)
 	{
-		float progress = Math.Clamp(0.5 * timeSlice, 0.0, 1.0);
-		return Math.Lerp(context.m_pVitals.GetHeartRate(), ComputeTargetHeartRate(context, timeSlice), progress);
+		float targetHeartRate = ComputeTargetHeartRate(context, timeSlice);
+		float prevHeartRate = context.m_pVitals.GetHeartRate();
+		float progress = Math.Min(0.5 * timeSlice, 1);
+		return Math.Lerp(prevHeartRate, targetHeartRate, progress);
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	protected float ComputeTargetHeartRate(ACE_Medical_CharacterContext context, float timeSlice)
 	{
-		float painAdjustedHR = s_pCirculationSettings.m_fDefaultHeartRateBPM + 50 * context.m_pDamageManager.ACE_Medical_GetPainIntensity();
-		float target = Math.Max(ComputeBaseTargetHeartRate(context, timeSlice), painAdjustedHR);
+		float target = ComputeBaseTargetHeartRate(context, timeSlice);
+		target = Math.Max(target, s_pCirculationSettings.m_fDefaultHeartRateBPM + 50 * context.m_pDamageManager.ACE_Medical_GetPainIntensity());
 		target += context.m_pVitals.GetHeartRateMedicationAdjustment();
 		// TO DO: SpO2-based adjustments
 		
-		return Math.Max(0.0, target);
+		return Math.Max(0, target);
 	}
 	
 	//------------------------------------------------------------------------------------------------

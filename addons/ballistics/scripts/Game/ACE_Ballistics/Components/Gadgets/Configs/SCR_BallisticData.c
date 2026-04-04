43e66ee0e9659314869cb614841c2ccc6af18f89
Kex <kexanone@gmail.com>
2026-04-04 23:17:08 +0200

    Store default zeroing range

--- addons/ballistics/scripts/Game/ACE_Ballistics/Components/Gadgets/Configs/SCR_BallisticData.c
+++ addons/ballistics/scripts/Game/ACE_Ballistics/Components/Gadgets/Configs/SCR_BallisticData.c
@@ -2,6 +2,7 @@
 modded class SCR_BallisticData : ScriptAndConfig
 {
 	protected SCR_EOpticsAngleUnits m_eACE_UnitType;
+	protected float m_eACE_DefaultZeroingRange;
 
 	//------------------------------------------------------------------------------------------------
 	void ACE_SetUnitType(SCR_EOpticsAngleUnits unitType)
@@ -13,5 +14,17 @@ modded class SCR_BallisticData : ScriptAndConfig
 	SCR_EOpticsAngleUnits ACE_GetUnitType()
 	{
 		return m_eACE_UnitType;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void ACE_SetDefaultZeroingRange(float range)
+	{
+		m_eACE_DefaultZeroingRange = range;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	float ACE_GetDefaultZeroingRange()
+	{
+		return m_eACE_DefaultZeroingRange;
 	}
 }

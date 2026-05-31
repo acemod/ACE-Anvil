5c74931b37d72ed5ef4c1844a4012c480590fd8f
minecraftLord77 <66845848+minecraftLord77@users.noreply.github.com>
2026-05-31 11:33:38 -0400

    beautiful, beautiful freezing effect

--- addons/ACE_Medical_Temperature/scripts/Game/1.ACE_Medical_Temperature/ACE_Temperature_FreezingEffectHandler.c
+++ addons/ACE_Medical_Temperature/scripts/Game/1.ACE_Medical_Temperature/ACE_Temperature_FreezingEffectHandler.c
@@ -1,31 +1,49 @@
-class ACE_Temperature_FreezingEffectHandler :SCR_InfoDisplayExtended
+class ACE_Temperature_FreezingEffectHandler :SCR_BaseScreenEffect
 {
-	
-	protected float m_fDeathProgress;
-	protected bool m_bEnabled;
 	protected ACE_Medical_VitalsComponent m_PlayerVitals;
 	protected float m_fDefaultCoreTemperature;
 	protected float m_fUnconTemperature;
+	
+	protected float m_fYTemperature;
+	protected float m_fXTemperature;
+	protected float m_fInterpProgress;
+	
+	
 	ImageWidget m_wImageWidget;
-	void InitEffect(SCR_ChimeraCharacter char,Widget root){
+	
+	override void DisplayControlledEntityChanged(IEntity from, IEntity to){
+		ClearEffects();
+
+		SCR_ChimeraCharacter char = SCR_ChimeraCharacter.Cast(to);
+		if (!char)
+			return;
+		
 		m_PlayerVitals=ACE_Medical_VitalsComponent.Cast(char.FindComponent(ACE_Medical_VitalsComponent));
-		m_wImageWidget=ImageWidget.Cast(root.FindAnyWidget("ACE_Temperature_Freezing"));
-		m_fDeathProgress=0;
+		m_wImageWidget=ImageWidget.Cast(m_wRoot.FindAnyWidget("ACE_Temperature_Freezing"));
 		m_fDefaultCoreTemperature=ACE_SettingsHelperT<ACE_Medical_Temperature_Settings>.GetModSettings().m_fDefaultCoreTemperature;
 		m_fUnconTemperature=ACE_SettingsHelperT<ACE_Medical_Circulation_Settings>.GetModSettings().m_CriticalThresholds.m_fTemperatureThreshold;
+		m_fYTemperature=m_fDefaultCoreTemperature;
+		m_fXTemperature=m_fDefaultCoreTemperature;
 	}
 	
 	void EnableEffect(bool enable)
 	{
 		m_wImageWidget.SetEnabled(enable);
 		m_wImageWidget.SetVisible(enable);
-		m_bEnabled=enable;
 	}
 	
-	void UpdateEffect(float timeSlice)
+	override void UpdateEffect(float timeSlice)
 	{
-		m_fDeathProgress=Math.InverseLerp(m_fDefaultCoreTemperature, m_fUnconTemperature, m_PlayerVitals.GetTemperature());
-		m_wImageWidget.SetOpacity(m_fDeathProgress);
+		if (m_PlayerVitals.GetTemperature()!=m_fXTemperature){
+			m_fInterpProgress=0;
+			m_fYTemperature=m_fXTemperature;
+			m_fXTemperature=m_PlayerVitals.GetTemperature();
+		}
+		float m_fCurrentTemperature = Math.Lerp(m_fYTemperature,m_fXTemperature,m_fInterpProgress);
+		m_fInterpProgress+=timeSlice;
+		float m_fDeathProgress=Math.InverseLerp(m_fDefaultCoreTemperature, m_fUnconTemperature, m_fCurrentTemperature);
+		
+		m_wImageWidget.SetOpacity(m_fDeathProgress/2+0.5);
 		m_wImageWidget.SetMaskProgress(m_fDeathProgress);
 	}
 	

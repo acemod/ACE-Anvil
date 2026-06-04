39b6e3c51cd4aa79998f764661d9ad2018d714a6
minecraftLord77 <66845848+minecraftLord77@users.noreply.github.com>
2026-06-04 16:13:12 -0400

    Pretty much done?

--- addons/carrying/Scripts/Game/ACE_Carrying/UserActions/ACE_Carrying_PullUserAction.c
+++ addons/carrying/Scripts/Game/ACE_Carrying/UserActions/ACE_Carrying_PullUserAction.c
@@ -1,4 +1,60 @@
-class ACE_Carrying_PullUserAction : ACE_Carrying_BaseUserAction {
+class ACE_Carrying_PullUserAction : ScriptedUserAction {
+	float m_fApplicationDeltaTime;
+	float m_fApplicationInterval=0.1;
+	float movementFactor = 0.4;
+	bool currentlyPulled;
+	SCR_CharacterAnimationComponent ownerAnimationComponent;
+	SCR_CharacterControllerComponent userControllerComponent;
+	vector boneMatrix[4];
+	[Attribute(desc:"0 crotch, 1 upper chest, 2 head, 3 left arm,4 right arm,5 left wrist,6 right wrist,7 left leg,8 left knee,9 right leg,10 right knee")]
+	int boneIndex;
+
+	override void PerformContinuousAction(IEntity pOwnerEntity, IEntity pUserEntity, float timeSlice)
+	{
+		//Make the user entity crouch, if they are not swimming
+		if (!userControllerComponent.IsSwimming())
+		{
+			userControllerComponent.ForceStance(ECharacterStance.CROUCH);//Put them into crouch position
+		}
+		//Enforce one call per interval
+		if (m_fApplicationDeltaTime<m_fApplicationInterval)
+		{
+			m_fApplicationDeltaTime+=timeSlice;
+			return;
+		}
+		m_fApplicationDeltaTime-=m_fApplicationInterval;
+		
+		ownerAnimationComponent.GetRagdollBone(boneIndex,boneMatrix);//Get the ragdoll bone
+		vector boneCoordinates = {boneMatrix[3][0],boneMatrix[3][1],boneMatrix[3][2]};//Find it's location
+		
+		vector userEntityPos  = pUserEntity.GetOrigin();
+
+		vector impulseDirection = (userEntityPos-boneCoordinates).Normalized();//Find the direction that impulse should be applied in
+		float m_fDistance  = Math.Min(vector.Distance(boneCoordinates,userEntityPos),2); //Cap it, so no infinite force
+		float impulsePower = Math.Pow(m_fDistance,2)*40-20;
+		ownerAnimationComponent.AddRagdollEffectorDamage(pOwnerEntity.CoordToLocal(boneCoordinates),impulseDirection,impulsePower,0.2,m_fApplicationInterval);//Apply a force to it
+	}
+
+	override void OnActionStart(IEntity pUserEntity)
+	{
+		super.OnActionStart(pUserEntity);
+		currentlyPulled=true;
+		
+		if (!ownerAnimationComponent)//The owner never changes, so it is safe to create once
+			ownerAnimationComponent = SCR_CharacterAnimationComponent.Cast(GetOwner().FindComponent(SCR_CharacterAnimationComponent));
+		
+		userControllerComponent = SCR_CharacterControllerComponent.Cast(pUserEntity.FindComponent(SCR_CharacterControllerComponent));
+		userControllerComponent.OverrideMaxSpeed(movementFactor);
+	}
+
+	override void OnActionCanceled(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		super.OnActionCanceled(pOwnerEntity, pUserEntity);
+		currentlyPulled=false;
+		userControllerComponent.OverrideMaxSpeed(1);
+		userControllerComponent.ForceStance(0);//Let them stand back up
+	}
+	
 	override bool CanBePerformedScript(IEntity user)
 	{
 		SCR_ChimeraCharacter userChar = SCR_ChimeraCharacter.Cast(user);
@@ -9,14 +65,16 @@ class ACE_Carrying_PullUserAction : ACE_Carrying_BaseUserAction {
 		SCR_CharacterControllerComponent userCharController = SCR_CharacterControllerComponent.Cast(userChar.GetCharacterController());
 		if (!userCharController || !userCharController.ACE_CanCarry(user,m_sCannotPerformReason))
 			return false;
+		
+		if (currentlyPulled && (userCharController != userControllerComponent))//if the current puller != this person, don't let them pull
+			return false;
 		//If you're not carrying something, you are allowed to pull something :)
 		return true;
-	
-	
 	}
+	
 	override bool CanBeShownScript(IEntity user)
 	{
-		//---Code from super, reused to maximize efficency (tm)---//
+		//---Code from ACE_Carrying_BaseUserAction, reused to maximize efficency (tm)---//
 		if (!super.CanBeShownScript(user))
 			return false;
 		
@@ -27,45 +85,21 @@ class ACE_Carrying_PullUserAction : ACE_Carrying_BaseUserAction {
 		SCR_CharacterControllerComponent ownerCharController = SCR_CharacterControllerComponent.Cast(ownerChar.GetCharacterController());
 		if (!ownerCharController || ownerCharController.GetLifeState() != ECharacterLifeState.INCAPACITATED)
 			return false;
-		
-		//---New code---//
-		//---If I am not ragdolling// or i am not in water
-		if (!ownerCharController.GetAnimationComponent().IsRagdollActive() || !ChimeraWorldUtils.TryGetWaterSurfaceSimple(GetGame().GetWorld(),ownerChar.GetOrigin()))
+		//---New code, check if not ragdolling
+		if (!ownerCharController.GetAnimationComponent().IsRagdollActive())
 			return false;
-
+		
 		return true;
 	}
+
 	
-	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	
+	override float GetActionProgressScript(float fProgress, float timeSlice)
 	{
-		vector ownerEntityPos = pOwnerEntity.GetOrigin();//Ignore Y component
-		vector userEntityPos  = pUserEntity.GetOrigin();
-		
-		vector distanceVector = userEntityPos-ownerEntityPos;
-		float distanceFloat = vector.Distance(ownerEntityPos,userEntityPos);
-		
-		float travelDistance = Math.Min(1,distanceFloat)-0.1; // Minimum buffer distance of a bit, cap 1 meter per drag
-		
-		
-		
-		float travelDistanceNormalized = travelDistance/distanceFloat; //Normalize by distance
-		vector newOwnerPos = vector.Lerp(ownerEntityPos,userEntityPos,travelDistanceNormalized);
-		newOwnerPos[2]=newOwnerPos[2]+0.7; //Correction factor to keep the ragdoll from clipping into the ground
-		Print(ownerEntityPos);
-		Print(userEntityPos);
-		Print(distanceVector);
-		Print(distanceFloat);
-		Print(travelDistance);
-		Print(travelDistanceNormalized);
-		
-		SCR_EditableCharacterComponent comp = SCR_EditableCharacterComponent.Cast(pOwnerEntity.FindComponent(SCR_EditableCharacterComponent));
-		vector transform[4];
-		pOwnerEntity.GetTransform(transform);
-		transform[3] = newOwnerPos;
-		comp.SetTransform(transform);
-		
-        
-        
-		
+		if (currentlyPulled)
+			return 998;
+		return 0;//Bar empty when not pulled
 	}
+
 }
+

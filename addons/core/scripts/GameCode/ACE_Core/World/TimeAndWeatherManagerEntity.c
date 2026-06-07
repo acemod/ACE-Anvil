009060f1e7196b8ec27103febfd5ae2d3bc4d65d
Kex <kexanone@gmail.com>
2026-06-07 08:21:45 +0200

    Update doc comments

--- addons/core/scripts/GameCode/ACE_Core/World/TimeAndWeatherManagerEntity.c
+++ addons/core/scripts/GameCode/ACE_Core/World/TimeAndWeatherManagerEntity.c
@@ -2,12 +2,21 @@
 modded class TimeAndWeatherManagerEntity : BaseTimeAndWeatherManagerEntity
 {
 	//------------------------------------------------------------------------------------------------
-	//! Converts time of the day in hours at 0° longitude (e.g. returned by GetTimeOfTheDay()) to current longitude
+	//! Converts time of the day at 0° longitude (e.g. returned by GetTimeOfTheDay()) to local time of 
+	//! the day for the current longitude
+	//! \param[in] time Time of the day in hours
+	//! \return local time of the day in hours
 	float ACE_ToLocalTimeOfTheDay(float time)
 	{
 		return SCR_Math.fmod(time + GetCurrentLongitude() / 15, 24);
 	}
 	
+	//------------------------------------------------------------------------------------------------
+	//! Adds offset in days to date
+	//! \param[inout] year Year of the date to update
+	//! \param[inout] month Month of the date to update
+	//! \param[inout] day Day of the date to update
+	//! \param[in] offset Offset in days (can be negative)
 	void ACE_AddDaysToDate(inout int year, inout int month, inout int day, int offset)
 	{
 	    int sign = offset.Sign();

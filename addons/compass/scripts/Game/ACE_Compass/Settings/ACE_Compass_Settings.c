63470fe66130c288fbe0362eb72aa39f62f6e0d8
Kex <kexanone@gmail.com>
2026-06-12 16:30:19 +0200

    Revert "Merge branch 'compass/declination' into weather/kestrel"
    This reverts commit 1e79e8f60b8ce4b0cf7789557e920b3c6c9008bb, reversing
    changes made to 14060a6d70dcc0c2d413a3882731c5fa7ddfadac.

--- addons/compass/scripts/Game/ACE_Compass/Settings/ACE_Compass_Settings.c
+++
@@ -1,7 +0,0 @@
-//------------------------------------------------------------------------------------------------
-[BaseContainerProps()]
-class ACE_Compass_Settings : ACE_ModSettings
-{
-	[Attribute(defvalue: "true", desc: "Enables simulation of magnatic north and makes compass point to it.")]
-	bool m_bMagneticDeclinationEnabled;
-}

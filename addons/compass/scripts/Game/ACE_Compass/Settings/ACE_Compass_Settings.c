1b8cbd150ef669b132f46c3485e069686040872c
Kex <kexanone@gmail.com>
2026-06-12 04:03:00 +0200

    Improve description of declination setting

--- addons/compass/scripts/Game/ACE_Compass/Settings/ACE_Compass_Settings.c
+++ addons/compass/scripts/Game/ACE_Compass/Settings/ACE_Compass_Settings.c
@@ -2,6 +2,6 @@
 [BaseContainerProps()]
 class ACE_Compass_Settings : ACE_ModSettings
 {
-	[Attribute(defvalue: "true", desc: "Whether compass should point to magnetic north instead of true north.")]
+	[Attribute(defvalue: "true", desc: "Enables simulation of magnatic north and makes compass point to it.")]
 	bool m_bMagneticDeclinationEnabled;
 }

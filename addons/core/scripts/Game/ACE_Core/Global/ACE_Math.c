a706b490ba66f7b8da539e5a9e1453b01f850778
Kex <kexanone@gmail.com>
2026-06-12 03:27:43 +0200

    Add bilinear interpolation

--- addons/core/scripts/Game/ACE_Core/Global/ACE_Math.c
+++ addons/core/scripts/Game/ACE_Core/Global/ACE_Math.c
@@ -25,4 +25,18 @@ class ACE_Math
 		float scale = Math.Pow(10, precision);
 		return Math.Round(value * scale) / scale;
 	}
+	
+	//------------------------------------------------------------------------------------------------
+	//! Does bilinear interpolation between `a`, `b`, `c` and `d` using `tx` and `ty`.
+	//! \param[in] a Value at (0, 0)
+	//! \param[in] b Value at (1, 0)
+	//! \param[in] c Value at (0, 1)
+	//! \param[in] d Value at (1, 1)
+	//! \return interpolated value
+	float Bilerp(float a, float b, float c, float d, float tx, float ty)
+	{
+		float y0 = Math.Lerp(a, b, tx);
+		float y1 = Math.Lerp(c, d, tx);
+		return Math.Lerp(y0, y1, ty);
+	}
 }

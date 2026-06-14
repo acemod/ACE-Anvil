b436bd792184bc2171fa7d4d74ad7edaf158fc72
Kex <kexanone@gmail.com>
2026-06-14 03:23:03 +0200

    Completely move mission header handling to system

--- addons/core/scripts/Game/ACE_Core/Systems/ACE_SettingsSystem.c
+++ addons/core/scripts/Game/ACE_Core/Systems/ACE_SettingsSystem.c
@@ -2,7 +2,6 @@
 class ACE_SettingsSystem : GameSystem
 {
 	static protected ref ACE_SettingsConfig s_CachedSettingsConfig;
-	static protected bool s_bWasMissionHeaderApplied = false;
 	
 	[RplProp()]
 	protected ref ACE_SettingsConfig m_SettingsConfig;
@@ -14,15 +13,6 @@ class ACE_SettingsSystem : GameSystem
 		return ACE_SettingsSystem.Cast(world.FindSystem(ACE_SettingsSystem));
 	}
 	
-	//------------------------------------------------------------------------------------------------
-	void ACE_SettingsSystem()
-	{
-		if (!s_CachedSettingsConfig)
-			s_CachedSettingsConfig = SCR_ConfigHelperT<ACE_SettingsConfig>.GetConfigObject("{A305FEB7400A2965}Configs/ACE/Settings.conf");
-		
-		m_SettingsConfig = s_CachedSettingsConfig;
-	}
-		
 	//------------------------------------------------------------------------------------------------
 	override static void InitInfo(WorldSystemInfo outInfo)
 	{
@@ -36,18 +26,18 @@ class ACE_SettingsSystem : GameSystem
 	//! Override with settings present in mission header
 	//! Only done the first time due to subsequent mission headers not containing server config settings
 	//! See https://feedback.bistudio.com/T172515
-	[Friend(ArmaReforgerScripted)]
-	static protected void OnMissionSet(MissionHeader mission)
+	void ACE_SettingsSystem()
 	{
-		if (s_bWasMissionHeaderApplied)
-			return;
-		
-		SCR_MissionHeader scriptedMission = SCR_MissionHeader.Cast(mission);
-		if (!scriptedMission)
-			return;
-		
-		scriptedMission.ACE_ApplyToSettingsConfig(s_CachedSettingsConfig);
-		s_bWasMissionHeaderApplied = true;
+		if (!s_CachedSettingsConfig)
+		{
+			s_CachedSettingsConfig = SCR_ConfigHelperT<ACE_SettingsConfig>.GetConfigObject("{A305FEB7400A2965}Configs/ACE/Settings.conf");
+			
+			SCR_MissionHeader mission = SCR_MissionHeader.Cast(GetGame().GetMissionHeader());
+			if (mission)
+				mission.ACE_ApplyToSettingsConfig(s_CachedSettingsConfig);
+		}
+
+		m_SettingsConfig = s_CachedSettingsConfig;
 	}
 
 	//------------------------------------------------------------------------------------------------

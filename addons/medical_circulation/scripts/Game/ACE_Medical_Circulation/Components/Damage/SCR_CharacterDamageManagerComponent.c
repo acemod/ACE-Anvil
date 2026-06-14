fbb55718566073f8b3b8edc706b7b2cd12a20418
Kex <kexanone@gmail.com>
2026-06-14 18:56:41 +0200

    Check for existance of brain HZ when updating resilience regen scale

--- addons/medical_circulation/scripts/Game/ACE_Medical_Circulation/Components/Damage/SCR_CharacterDamageManagerComponent.c
+++ addons/medical_circulation/scripts/Game/ACE_Medical_Circulation/Components/Damage/SCR_CharacterDamageManagerComponent.c
@@ -77,7 +77,8 @@ modded class SCR_CharacterDamageManagerComponent : SCR_DamageManagerComponent
 			m_fACE_Medical_ResilienceRegenScale = s_ACE_Medical_Core_Settings.m_fDefaultResilienceRegenScale;
 		
 		// Scale recovery scale by health of the brain hit zone
-		m_fACE_Medical_ResilienceRegenScale *= m_ACE_Medical_BrainHitZone.GetHealthScaled();
+		if (m_ACE_Medical_BrainHitZone)
+			m_fACE_Medical_ResilienceRegenScale *= m_ACE_Medical_BrainHitZone.GetHealthScaled();
 	}
 	
 	//-----------------------------------------------------------------------------------------------------------

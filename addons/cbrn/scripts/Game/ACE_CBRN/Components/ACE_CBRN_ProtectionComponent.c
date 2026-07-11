8f4c0667e308a27f42e58736de03f1acc3a041b8
Kex <kexanone@gmail.com>
2026-07-11 23:48:26 +0200

    Add protection component

---
+++ addons/cbrn/scripts/Game/ACE_CBRN/Components/ACE_CBRN_ProtectionComponent.c
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------------------------
+class ACE_CBRN_ProtectionComponentClass : ScriptComponentClass
+{
+}
+
+//------------------------------------------------------------------------------------------------
+//! Added to equipment for designating them as protective
+class ACE_CBRN_ProtectionComponent : ScriptComponent
+{
+}

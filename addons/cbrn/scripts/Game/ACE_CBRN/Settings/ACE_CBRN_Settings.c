1c537d5f33918b0c42840740f81f46d35695a7c2
Kex <kexanone@gmail.com>
2026-07-18 03:09:29 +0200

    Add settings

---
+++ addons/cbrn/scripts/Game/ACE_CBRN/Settings/ACE_CBRN_Settings.c
@@ -0,0 +1,7 @@
+//------------------------------------------------------------------------------------------------
+[BaseContainerProps()]
+class ACE_CBRN_Settings : ACE_ModSettings
+{
+	[Attribute(defvalue: "false", desc: "AI will die instead of becoming paralyzed if false")]
+	float m_bFullParalysisForAIEnabled;
+}

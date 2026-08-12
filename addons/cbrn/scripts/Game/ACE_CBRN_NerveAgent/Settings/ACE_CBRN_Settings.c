b04d34163f1befa380d56cd03ad40ca0fe8ec0e5
Kex <kexanone@gmail.com>
2026-08-12 23:01:01 +0200

    Fix settings type

--- addons/cbrn/scripts/Game/ACE_CBRN_NerveAgent/Settings/ACE_CBRN_Settings.c
+++ addons/cbrn/scripts/Game/ACE_CBRN_NerveAgent/Settings/ACE_CBRN_Settings.c
@@ -3,5 +3,5 @@
 modded class ACE_CBRN_Settings : ACE_ModSettings
 {
 	[Attribute(defvalue: "false", desc: "AI will die instead of becoming paralyzed if false")]
-	float m_bFullParalysisForAIEnabled;
+	bool m_bFullParalysisForAIEnabled;
 }

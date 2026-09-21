65ab73967104bf4a8693cf4ccc3bc2d5dab8a3d8
Kex <kexanone@gmail.com>
2026-09-21 20:24:08 +0200

    Add system for spawning/despawning helpers

---
+++ addons/core/scripts/Game/ACE_Core/Systems/ACE_ActionsHelperEntitySystem.c
@@ -0,0 +1,167 @@
+//------------------------------------------------------------------------------------------------
+//! Handles dynamic spawning/despawning of user actions helper entities
+class ACE_ActionsHelperEntitySystem : GameSystem
+{
+	[Attribute(defvalue: "0.5", desc: "Timeout between updates [s]")]
+	protected float m_fUpdateTimeout;
+	
+	[Attribute(defvalue: "5.0", desc: "Radius in meters for searching entities to spawn helpers on.")]
+	protected float m_fEntityDetectionRadius;
+	
+	protected ref array<ref ACE_ActionsHelperEntityGroupContext> m_aContexts = {};
+	protected ref array<ref ACE_ActionsHelperEntityConfig> m_aDeletedConfigs = {};
+	protected bool m_bUpdating = false;
+	protected float m_fUpdateTimer;
+	
+	//------------------------------------------------------------------------------------------------
+	static ACE_ActionsHelperEntitySystem GetInstance(ChimeraWorld world)
+	{
+		return ACE_ActionsHelperEntitySystem.Cast(world.FindSystem(ACE_ActionsHelperEntitySystem));
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override static void InitInfo(WorldSystemInfo outInfo)
+	{
+		outInfo.SetAbstract(false)
+			.SetUnique(true)
+			.SetLocation(WorldSystemLocation.Client)
+			.AddPoint(WorldSystemPoint.Frame);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override protected void OnInit()
+	{
+		Enable(!m_aContexts.IsEmpty());
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override protected void OnStarted()
+	{
+		m_fUpdateTimer = 0;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override protected void OnUpdatePoint(WorldUpdatePointArgs args)
+	{
+		m_fUpdateTimer -= args.GetTimeSliceSeconds();
+		if (m_fUpdateTimer > 0)
+			return;
+		
+		m_fUpdateTimer = m_fUpdateTimeout;
+		
+		IEntity player = SCR_PlayerController.GetLocalControlledEntity();
+		if (!player)
+			return;
+		
+		m_bUpdating = true;
+		
+		GetWorld().QueryEntitiesBySphere(player.GetOrigin(), m_fEntityDetectionRadius, OnEntityDetected);
+		
+		foreach (ACE_ActionsHelperEntityGroupContext context : m_aContexts)
+		{
+			// Delete helpers no longer in range
+			for (int i = context.m_aEntitiesInRange.Count() - 1; i >= 0; i--)
+			{
+				IEntity entity = context.m_aEntitiesInRange[i];
+				if (!entity || !context.m_aNewEntitiesInRange.Contains(entity))
+				{
+					SCR_EntityHelper.DeleteEntityAndChildren(context.m_aHelperEntities[i]);
+					context.m_aEntitiesInRange.Remove(i);
+					context.m_aHelperEntities.Remove(i);
+				}
+			}
+			
+			// Create new helpers in range
+			foreach (IEntity newEntity : context.m_aNewEntitiesInRange)
+			{
+				if (newEntity && !context.m_aEntitiesInRange.Contains(newEntity))
+				{
+					context.m_aHelperEntities.Insert(context.m_Config.SpawnHelper(newEntity));
+					context.m_aEntitiesInRange.Insert(newEntity);
+				}
+			}
+			
+			context.m_aNewEntitiesInRange.Clear();
+		}
+		
+		m_bUpdating = false;
+		
+		foreach (ACE_ActionsHelperEntityConfig config: m_aDeletedConfigs)
+		{
+			Unregister(config);
+		}
+		
+		m_aDeletedConfigs.Clear();
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected bool OnEntityDetected(IEntity ent)
+	{
+		foreach (ACE_ActionsHelperEntityGroupContext context : m_aContexts)
+		{
+			if (!context.m_Config.ValidateEntity(ent))
+				continue;
+			
+			context.m_aNewEntitiesInRange.Insert(ent);
+			break;
+		}
+		
+		return true;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void Register(ACE_ActionsHelperEntityConfig config)
+	{
+		foreach (ACE_ActionsHelperEntityGroupContext context : m_aContexts)
+		{
+			if (context.m_Config == config)
+				return;
+		}
+		
+		m_aContexts.Insert(new ACE_ActionsHelperEntityGroupContext(config));
+		Enable(true);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void Unregister(ACE_ActionsHelperEntityConfig config)
+	{
+		if (m_bUpdating)
+		{
+			m_aDeletedConfigs.Insert(config);
+		}
+		else
+		{
+			for (int i = m_aContexts.Count() - 1; i >= 0; i--)
+			{
+				if (m_aContexts[i].m_Config != config)
+					continue;
+				
+				foreach (IEntity helper : m_aContexts[i].m_aHelperEntities)
+				{
+					SCR_EntityHelper.DeleteEntityAndChildren(helper);
+				}
+				
+				m_aContexts.Remove(i);
+				break;
+			}
+			
+			if (m_aContexts.IsEmpty())
+				Enable(false);
+		}
+	}
+}
+
+//------------------------------------------------------------------------------------------------
+class ACE_ActionsHelperEntityGroupContext : Managed
+{
+	ACE_ActionsHelperEntityConfig m_Config;
+	ref array<IEntity> m_aEntitiesInRange = {};
+	ref array<IEntity> m_aNewEntitiesInRange = {};
+	ref array<IEntity> m_aHelperEntities = {};
+	
+	//------------------------------------------------------------------------------------------------
+	void ACE_ActionsHelperEntityGroupContext(ACE_ActionsHelperEntityConfig config)
+	{
+		m_Config = config;
+	}
+}
